5abcffb6c40e7de0fb1cd4579f85e5cea25af295
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-09-17 12:45:43 +0330

    Animated, PulseDot

--- Clients/Frontend-Reactjs/src/presentation/pages/Home/index.tsx
+++ Clients/Frontend-Reactjs/src/presentation/pages/Home/index.tsx
@@ -1,43 +1,7 @@
 import { Stack } from "@mui/material";
-import Alert from "../../components/Alert";
 
 const Home = () => {
-  return (
-    <Stack gap={8}>
-      <Alert
-        variant="success"
-        body="This is a filled success Alert."
-        isToast={true}
-        onClick={() => console.log("Click")}
-        title="Success"
-        hasIcon={true}
-      />
-      <Alert
-        variant="info"
-        body="This is a filled info Alert."
-        isToast={true}
-        onClick={() => console.log("Click")}
-        title="info"
-        hasIcon={true}
-      />
-      <Alert
-        variant="error"
-        body="This is a filled error Alert."
-        isToast={true}
-        onClick={() => console.log("Click")}
-        title="error"
-        hasIcon={true}
-      />
-      <Alert
-        variant="warning"
-        body="This is a filled warning Alert."
-        isToast={true}
-        onClick={() => console.log("Click")}
-        title="warning"
-        hasIcon={true}
-      />
-    </Stack>
-  );
+  return <Stack gap={8}></Stack>;
 };
 
 export default Home;

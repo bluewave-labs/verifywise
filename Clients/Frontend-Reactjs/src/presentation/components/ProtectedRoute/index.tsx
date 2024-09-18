46d6dbdd4e070af1e25ca734a3e8db4ea3df1c50
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-09-18 22:05:49 +0330

    ProtectedRoute

--- Clients/Frontend-Reactjs/src/presentation/components/ProtectedRoute/index.tsx
+++ Clients/Frontend-Reactjs/src/presentation/components/ProtectedRoute/index.tsx
@@ -1,3 +1,16 @@
+/**
+ * ProtectedRoute component is a higher-order component that wraps around
+ * another component to provide route protection based on authentication state.
+ * It checks if the user is authenticated by inspecting the authToken from the
+ * Redux store. If the user is authenticated, it renders the wrapped component.
+ * Otherwise, it redirects the user to the login page.
+ *
+ * @component
+ * @param {ComponentType<any>} Component - The component to be rendered if the user is authenticated.
+ * @param {object} rest - Additional props to be passed to the wrapped component.
+ * @returns {JSX.Element} - The rendered component if authenticated, or a redirect to the login page.
+ */
+
 import "./index.css";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
--- Clients/Frontend-Reactjs/src/presentation/pages/Home/index.tsx
+++ Clients/Frontend-Reactjs/src/presentation/pages/Home/index.tsx
@@ -1,7 +1,12 @@
 import { Stack } from "@mui/material";
+import Search from "../../components/Search";
 
 const Home = () => {
-  return <Stack gap={8}></Stack>;
+  return (
+    <Stack gap={8}>
+      <Search />
+    </Stack>
+  );
 };
 
 export default Home;

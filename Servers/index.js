1d0fd79fa38c175e4c183cb03553adf4b89c89ea
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-09-21 11:09:15 +0330

    Server side template code is ready

--- Servers/index.js
+++ Servers/index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", async (req, res) => {
-  res.send("Hello, world");
-});
+const cors = require("cors");
+
+const userRouter = require("./routes/users.route");
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/users", userRouter);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port http://localhost:${port}/`);
 });

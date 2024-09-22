b1511c32ec69b045f858a57bbde4088cc015aacf
Harsh <pharsh4585@gmail.com>
2024-09-22 01:20:06 -0400

    initial commit for adding user route

--- Servers/index.js
+++ Servers/index.js
@@ -5,10 +5,12 @@ const port = 3000;
 const cors = require("cors");
 
 const userRouter = require("./routes/users.route");
+const bodyParser = require("body-parser");
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json())
 
 // Routes
 app.use("/users", userRouter);

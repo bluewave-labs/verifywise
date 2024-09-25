da08a12024da2ae65c4453d370678b233b4e9192
Harsh <pharsh4585@gmail.com>
2024-09-25 17:02:36 -0400

    added code to use previous structure for the app and migrated to typescript

--- Servers/interactors/iUser.interactor.ts
+++
@@ -1,7 +0,0 @@
-export interface IUserInteractor {
-    getAll();
-    getOne(id: number);
-    create(input: any);
-    delete(id: number);
-    update(id: number, input: any);
-}
---
+++ Servers/src/models/user.model.ts
@@ -0,0 +1,9 @@
+// User model
+export class User {
+    constructor(
+        public id: number,
+        public name: string,
+        public email: string,
+        public password: string
+    ) { }
+}

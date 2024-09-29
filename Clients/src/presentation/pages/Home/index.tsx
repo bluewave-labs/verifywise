4ca5e5cbeee79a82862a25c3bd13ee2f995ee79c
Yevheniia Popova <popovaevg18@gmail.com>
2024-09-29 16:44:56 -0600

    Added 'All projects compliance status' block and 'All projects risk status' block

--- Clients/src/presentation/pages/Home/index.tsx
+++ Clients/src/presentation/pages/Home/index.tsx
@@ -5,36 +5,72 @@ import { mockProjects } from "./projectData";
 import { styles } from "./styles";
 
 interface HomeProps {
-    projects?: ProjectCardProps[];
+    projects?: ProjectCardProps[]
 }
 
 const Home = ({projects = mockProjects}: HomeProps) => {
+  const NoProjectsMessage  = () => (
+    <Box sx={styles.noProjectBox}>
+      <Box sx={{ display: "flex", justifyContent: "center" }}></Box>
+      <Typography sx={{textAlign: "center", mt: 13.5, color: "#475467" }}>
+        You have no projects, yet. Click on the "New Project" button to start one.
+      </Typography>
+    </Box>
+  );
+
   return (
     <Box sx={{ mt: 47, ml: 15.5, mr: 62 }}>
       <Box sx={styles.projectBox}>
-          <Typography variant="h1" component="div" sx={styles.tytle}>
-              Projects overview
-          </Typography>
-          <Button variant="contained" sx={{ textTransform: "none", borderRadius: 2, maxHeight: 34 }} onClick={() => {}}>
-              New project
-          </Button>
+        <Typography variant="h1" component="div" sx={styles.tytle}>
+          Projects overview
+        </Typography>
+        <Button variant="contained" sx={{ textTransform: "none", borderRadius: 2, maxHeight: 34 }} onClick={() => {}}>
+          New project
+        </Button>
       </Box>
-      <Grid container spacing={10} sx={{ display: "flex", justifyContent: "space-between" }}>
-          {projects && projects.length > 0 ? (
-              projects.map((item: ProjectCardProps) => (
-                  <Grid key={item.id}>
-                      <ProjectCard {...item} />
-                  </Grid>
-                  ))
-              ) : (
-                <Box sx={styles.noProjectBox}>
-                  <Box sx={{ display: "flex", justifyContent: "center" }}></Box>
-                  <Typography sx={{textAlign: "center", mt: 13.5, color: "#475467" }}>
-                    You have no projects, yet. Click on the “New Project” button to start one.
-                  </Typography>
-                </Box>
-          )} 
-      </Grid>
+      {projects && projects.length > 0 ? (
+        <>
+          <Grid container spacing={10} sx={{ display: "flex", justifyContent: "space-between" }}>
+            {projects.map((item: ProjectCardProps) => (
+                <Grid key={item.id}>
+                  <ProjectCard {...item} />
+                </Grid>
+            ))}
+          </Grid>
+
+          <Typography variant="h2" component="div" sx={styles.tytle2}>All projects compliance status</Typography>
+          <Grid container spacing={10} sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Grid sx={styles.grid}>
+              <Typography sx={styles.gridTytle}>Completed requirements</Typography>
+              <Typography sx={styles.gridValue}>85%</Typography>
+            </Grid>
+            <Grid sx={styles.grid}>
+              <Typography sx={styles.gridTytle}>Completed assessments</Typography>
+              <Typography sx={styles.gridValue}>24</Typography>
+            </Grid>
+            <Grid sx={styles.grid}>
+              <Typography sx={styles.gridTytle}>Assessment completion rate</Typography>
+              <Typography sx={styles.gridValue}>10%</Typography>
+            </Grid>
+          </Grid>
+
+          <Typography variant="h2" component="div" sx={styles.tytle2}>All projects risk status</Typography>
+          <Grid container spacing={10} sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Grid sx={styles.grid}>
+              <Typography sx={styles.gridTytle}>Acceptable risks</Typography>
+              <Typography sx={styles.gridValue}>31</Typography>
+            </Grid>
+            <Grid sx={styles.grid}>
+              <Typography sx={styles.gridTytle}>Residual risks</Typography>
+              <Typography sx={styles.gridValue}>1</Typography>
+            </Grid>
+            <Grid sx={styles.grid}>
+              <Typography sx={styles.gridTytle}>Unacceptable risks</Typography>
+              <Typography sx={styles.gridValue}>14</Typography>
+            </Grid>
+          </Grid>
+        </>
+      ) : <NoProjectsMessage /> } 
     </Box>
   )
 };
--- Clients/src/presentation/pages/Home/styles.ts
+++ Clients/src/presentation/pages/Home/styles.ts
@@ -1,8 +1,8 @@
 export const styles = {
     tytle: { 
-        color: '#1A1919', 
-        fontSize: 16, 
-        fontWeight: "bold" 
+      color: '#1A1919', 
+      fontSize: 16, 
+      fontWeight: 600 
     },
     projectBox: {
       display: "flex", 
@@ -18,5 +18,30 @@ export const styles = {
       borderRadius: 2, 
       pt: 34, 
       pb: 39.5
+    },
+    tytle2: {
+      color: '#1A1919', 
+      fontSize: 16, 
+      fontWeight: 500,
+      pl: 5,
+      pb: 8.5,
+      mt: 17
+    },
+    grid: {
+      border: "1px solid #EAECF0", 
+      borderRadius: 2, 
+      minWidth: 300, 
+      paddingX: 9.25, 
+      pt: 4, 
+      pb: 4.7 
+    },
+    gridTytle: {
+      fontSize: 12, 
+      color: "#8594AC", 
+      pb: 1.5 
+    },
+    gridValue: {
+      fontSize: 16, 
+      fontWeight: 600
     }
 }

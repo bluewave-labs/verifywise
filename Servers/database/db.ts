66fbcc1d3fe84d46c84c676c29bda0bbe00b4b09
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-10-01 17:05:55 +0330

    small modifications

---
+++ Servers/database/db.ts
@@ -0,0 +1,11 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "postgres",
+  port: Number(process.env.DB_PORT) || 5432,
+  password: process.env.DB_PASSWORD || "1377",
+  database: process.env.DB_NAME || "verifywise",
+});
+
+export default pool;

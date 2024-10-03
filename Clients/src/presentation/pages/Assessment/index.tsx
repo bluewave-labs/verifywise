9917a8d6789aae42afb2a09a33432fbcb1360e90
Matthew Sharp <matthew.sharp1@ontariotechu.net>
2024-10-03 11:38:52 -0400

    Converted Hard Coded Colours to Theme

--- Clients/src/presentation/pages/Assessment/index.tsx
+++ Clients/src/presentation/pages/Assessment/index.tsx
@@ -1,63 +1,66 @@
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import { Paper, styled, Typography, useTheme } from "@mui/material";
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#f9fafc',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  border:"1px solid #EAECF0",
-  boxShadow: "none",
-  paddingRight: "150px",
-  paddingLeft: "25px",
-  paddingTop: "10px",
-  paddingBottom: "10px",
-}));
+import { Stack, Button, Typography, useTheme, styled, Paper } from "@mui/material";
 
 const Assessment = () => {
   const theme = useTheme();
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.background.alt,
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    border: "1px solid",
+    borderColor: theme.palette.border,
+    boxShadow: "none",
+    paddingRight: "150px",
+    paddingLeft: "25px",
+    paddingTop: "10px",
+    paddingBottom: "10px",
+  }));
+
+  const buttonStyles = {
+    width: "157px",
+    height: "34px",
+    marginTop: "30px",
+    textTransform: 'none',
+    border: "1px solid",
+    borderColor: theme.palette.border,
+    borderRadius: "4px",
+    boxShadow: "none",
+    gap: "8px",
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    paddingLeft: "16px",
+    paddingRight: "16px",
+  }
+
   return (
     <div className="assessment-page">
-      <Stack gap={theme.spacing(2)} sx={{marginTop: "50px", marginLeft: "30px"}}>
-        <Typography variant="h5" fontWeight={"bold"} fontSize={"16px"} color='#1A1919' fontFamily={"inter"}>
+      <Stack gap={theme.spacing(2)} sx={{ marginTop: "50px", marginLeft: "30px" }}>
+        <Typography variant="h5" fontWeight={"bold"} fontSize={"16px"} color={theme.palette.text.primary} fontFamily={"inter"}>
           Assessment tracker
         </Typography>
-        <Stack direction={"row"} spacing={10} sx={{marginTop: "20px"}}>
+        <Stack direction={"row"} gap={theme.spacing(10)} sx={{ marginTop: "20px" }}>
           <Item>
-            <Typography sx={{fontSize:"12px", color:"#A0AEC0"}} fontFamily={"inter"}>Assessment completion</Typography>
-            <Typography fontWeight={"bold"} fontSize={"16px"} color='#2D3748' fontFamily={"inter"}>85%</Typography>
-          </Item>      
-          <Item sx={{fontSize:"12px"}}>
-            <Typography sx={{fontSize:"12px", color:"#A0AEC0"}} fontFamily={"inter"}>Pending assessments</Typography>
-            <Typography fontWeight={"bold"} fontSize={"16px"} color='#2D3748' fontFamily={"inter"}>2</Typography>
+            <Typography fontSize={"12px"} color={theme.palette.text.accent} fontFamily={"inter"}>Assessment completion</Typography>
+            <Typography fontWeight={"bold"} fontSize={"16px"} color={theme.palette.text.primary} fontFamily={"inter"}>85%</Typography>
           </Item>
-          <Item sx={{fontSize:"12px"}}>
-            <Typography sx={{fontSize:"12px", color:"#A0AEC0"}} fontFamily={"inter"}>Approved assessments</Typography>
-            <Typography fontWeight={"bold"} fontSize={"16px"} color='#2D3748' fontFamily={"inter"}>12</Typography>
+          <Item>
+            <Typography fontSize={"12px"} color={theme.palette.text.accent} fontFamily={"inter"}>Pending assessments</Typography>
+            <Typography fontWeight={"bold"} fontSize={"16px"} color={theme.palette.text.primary} fontFamily={"inter"}>2</Typography>
+          </Item>
+          <Item>
+            <Typography fontSize={"12px"} color={theme.palette.text.accent} fontFamily={"inter"}>Approved assessments</Typography>
+            <Typography fontWeight={"bold"} fontSize={"16px"} color={theme.palette.text.primary} fontFamily={"inter"}>12</Typography>
           </Item>
         </Stack>
-        <Typography variant="h5" fontWeight={"bold"} fontSize={"16px"} color='#1A1919' sx={{marginTop: "50px"}} fontFamily={"inter"}>
+        <Typography variant="h5" fontWeight={"bold"} fontSize={"16px"} color={theme.palette.text.primary} sx={{ marginTop: "50px" }} fontFamily={"inter"}>
           Ongoing assessments
         </Typography>
-        <Typography fontSize={"14px"} fontFamily={"inter"} color='#344054'>
+        <Typography fontSize={"14px"} fontFamily={"inter"} color={theme.palette.text.secondary}>
           Those are the assessments you started. Each assessment has a completion
           status on the left hand side of the table.
         </Typography>
         <Stack>
-          <Button variant='contained' size='medium' style={{
-            width: "157px",
-            height: "34px",
-            marginTop: "30px",
-            textTransform: 'none',
-            border:"1px solid #175CD3",
-            borderRadius:"4px",
-            boxShadow:"none",
-            gap: "8px",
-            paddingTop:"10px",
-            paddingBottom:"10px",
-            paddingLeft:"16px",
-            paddingRight:"16px",
-            }}>
+          <Button variant='contained' size='medium' sx={buttonStyles}>
             <Typography fontFamily={"inter"} fontSize={"13px"} fontWeight={"400"} lineHeight={"20px"} textAlign={"left"}>
               Go to assessments
             </Typography>
@@ -65,7 +68,7 @@ const Assessment = () => {
         </Stack>
       </Stack>
     </div>
-    
+
   );
 };
 

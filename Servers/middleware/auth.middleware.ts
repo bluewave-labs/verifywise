50ba180a4f6f139997e103c7c1e010e3ee99ab8f
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-10-04 19:23:57 +0330

    Status codes added

--- Servers/middleware/auth.middleware.ts
+++ Servers/middleware/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { getTokenPayload } from "../utils/jwt.util";
+import { STATUS_CODE } from "../utils/statusCode.utils";
 
 const authenticateJWT = async (
   req: Request,
@@ -9,20 +10,31 @@ const authenticateJWT = async (
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(400).json(
+      STATUS_CODE[400]({
+        message: "Token not found",
+      })
+    );
   }
 
   try {
     const decoded = getTokenPayload(token);
 
-    if (!decoded) return res.status(403).json({ message: "Not authorized" });
+    if (!decoded)
+      return res.status(401).json(
+        STATUS_CODE[401]({
+          message: "Unauthorized",
+        })
+      );
 
     if (decoded.expire < Date.now())
-      return res.status(401).json({ message: "Token expired" });
+      return res
+        .status(406)
+        .json(STATUS_CODE[406]({ message: "Token expired" }));
 
     next();
   } catch (error) {
-    return res.status(500).json({ message: "Server error" });
+    return res.status(500).json(STATUS_CODE[500]((error as Error).message));
   }
 };
 

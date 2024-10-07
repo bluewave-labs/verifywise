eafb8c287eec194bb737924ce485409a1710afab
Matthew Sharp <matthew.sharp1@ontariotechu.net>
2024-10-07 11:35:35 -0400

    Revert "Styling Updates"
    This reverts commit eaf9981423d1e0feb185817dc44464bd33d7d3d4.

--- Clients/src/presentation/pages/Assessment/index.tsx
+++ Clients/src/presentation/pages/Assessment/index.tsx
@@ -5,20 +5,17 @@ const Assessment = () => {
   const theme = useTheme();
 
   const paperStyle = {
-    backgroundColor: theme.palette.background.main,
+    backgroundColor: theme.palette.background.alt,
     ...theme.typography.body2,
     padding: theme.spacing(1),
     border: "1px solid",
-    borderColor: theme.palette.border.light,
+    borderColor: theme.palette.border,
     boxShadow: "none",
     paddingRight: "150px",
     paddingLeft: "25px",
     paddingTop: "10px",
     paddingBottom: "10px",
-    width: 'calc(100% - 150px - 25px)',
-    minWidth: '300px',
-    maxWidth: '80%',
-  };
+  }
 
   const buttonStyles = {
       width: "157px",
@@ -27,7 +24,7 @@ const Assessment = () => {
       textTransform: 'none',
       border: "1px solid",
       borderColor: theme.palette.status.info.border,
-      borderRadius: theme.shape.borderRadius,
+      borderRadius: "4px",
       boxShadow: "none",
       gap: "8px",
       paddingTop: "10px",
@@ -38,11 +35,11 @@ const Assessment = () => {
 
   return (
     <div className="assessment-page">
-      <Stack gap={theme.spacing(2)} sx={{backgroundColor: theme.palette.background.alt}}>
+      <Stack gap={theme.spacing(2)} sx={{ marginTop: "50px", marginLeft: "30px" }}>
         <Typography variant="h5" fontWeight={"bold"} fontSize={"16px"} color={theme.palette.text.primary} fontFamily={"inter"}>
           Assessment tracker
         </Typography>
-        <Stack direction={"row"} justifyContent={"space-between"} display={'flex'} gap={theme.spacing(10)} sx={{ marginTop: "20px" }}>
+        <Stack direction={"row"} gap={theme.spacing(10)} sx={{ marginTop: "20px" }}>
           <Paper sx={paperStyle}>
             <Typography fontSize={"12px"} color={theme.palette.text.accent} fontFamily={"inter"}>Assessment completion</Typography>
             <Typography fontWeight={"bold"} fontSize={"16px"} color={theme.palette.text.primary} fontFamily={"inter"}>85%</Typography>

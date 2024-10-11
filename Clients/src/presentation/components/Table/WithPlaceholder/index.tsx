807a67bee50f2760afc90ac4cf6b07824031b988
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-10-11 20:23:01 +0330

    Remove back and save buttons in the vendors page

--- Clients/src/presentation/components/Table/WithPlaceholder/index.tsx
+++ Clients/src/presentation/components/Table/WithPlaceholder/index.tsx
@@ -1,3 +1,22 @@
+/**
+ * TableWithPlaceholder component renders a table with a placeholder image when no data is provided.
+ * It displays a table header and a body populated with rows from the provided data.
+ *
+ * @component
+ * @param {Object} props - The properties object.
+ * @param {Array} props.data - The array of data objects to be displayed in the table. Defaults to listOfVendors.
+ * @returns {JSX.Element} The rendered TableWithPlaceholder component.
+ *
+ * @example
+ * // Example usage of TableWithPlaceholder
+ * const data = [
+ *   { name: "Vendor A", type: "Type 1", assignee: "John Doe", status: "Active", risk: "Low", review_date: "2023-01-01" },
+ *   { name: "Vendor B", type: "Type 2", assignee: "Jane Smith", status: "Inactive", risk: "High", review_date: "2023-02-01" }
+ * ];
+ *
+ * <TableWithPlaceholder data={data} />
+ */
+
 import {
   Table,
   TableBody,
@@ -11,23 +30,121 @@ import {
 import Placeholder from "../../../assets/imgs/table placeholder 1.png";
 import listOfVendors from "../../../mocks/vendors.data";
 import IconButton from "../../IconButton";
-import AddNewVendor from "../../Modals/NewVendor";
-import { useState } from "react";
 
-function TableWithPlaceholder({ data = listOfVendors }) {
+/**
+ * An array of strings representing the titles of the table columns.
+ * The columns include:
+ * - "name": The name of the item.
+ * - "type": The type of the item.
+ * - "assignee": The person assigned to the item.
+ * - "status": The current status of the item.
+ * - "risk": The risk level associated with the item.
+ * - "review date": The date when the item was last reviewed.
+ * - "": An empty string for a column with no title.
+ */
+const titleOfTableColumns = [
+  "name",
+  "type",
+  "assignee",
+  "status",
+  "risk",
+  "review date",
+  "",
+];
+
+const TableWithPlaceholder = ({ data = listOfVendors }) => {
   const theme = useTheme();
-
-  const [isOpen, setIsOpen] = useState(false);
-  const [value, setValue] = useState("1");
-
-  const handleChange = (_: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
 
   const cellStyle = { fontSize: 13, paddingY: theme.spacing(6) };
 
+  /**
+   * Renders the table header with specified styles and column titles.
+   *
+   * @constant
+   * @type {JSX.Element}
+   *
+   * @remarks
+   * The table header is styled with a light background color and uppercase text.
+   * Each column title is styled with a specific color, font weight, and padding.
+   *
+   * @param {Array<string>} titleOfTableColumns - An array of strings representing the titles of the table columns.
+   *
+   * @returns {JSX.Element} The table header component.
+   */
+  const tableHeader: JSX.Element = (
+    <TableHead
+      sx={{
+        backgroundColor: "#FAFAFA",
+      }}
+    >
+      <TableRow
+        sx={{
+          textTransform: "uppercase",
+          borderBottom: "1px solid #EEEEEE",
+        }}
+      >
+        {titleOfTableColumns.map((cell, index) => (
+          <TableCell
+            style={{
+              color: "#a1afc6",
+              fontWeight: 400,
+              paddingLeft: "16px",
+            }}
+            key={index}
+          >
+            {cell}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+
+  /**
+   * Renders the body of a table with rows populated from the provided data.
+   * Each row displays various properties of the data object such as name, type, assignee, status, risk, and review date.
+   * An IconButton is also rendered in the last cell of each row.
+   *
+   * @constant
+   * @type {JSX.Element}
+   * @param {Array} data - The array of data objects to be displayed in the table.
+   * @param {Object} row - The individual data object representing a row in the table.
+   * @param {number} index - The index of the current row in the data array.
+   * @param {Object} row.name - The name property of the data object.
+   * @param {Object} row.type - The type property of the data object.
+   * @param {Object} row.assignee - The assignee property of the data object.
+   * @param {Object} row.status - The status property of the data object.
+   * @param {Object} row.risk - The risk property of the data object.
+   * @param {Object} row.review_date - The review date property of the data object.
+   * @param {Object} cellStyle - The style object applied to each TableCell.
+   * @returns {JSX.Element} The rendered table body with rows.
+   */
+  const tableBody: JSX.Element = (
+    <TableBody>
+      {data &&
+        data.map((row, index) => (
+          <TableRow
+            key={index}
+            sx={{
+              textTransform: "capitalize",
+              borderBottom: "1px solid #EEEEEE",
+            }}
+          >
+            <TableCell sx={cellStyle}>{row.name}</TableCell>
+            <TableCell sx={cellStyle}>{row.type}</TableCell>
+            <TableCell sx={cellStyle}>{row.assignee}</TableCell>
+            <TableCell sx={cellStyle}>{row.status}</TableCell>
+            <TableCell sx={cellStyle}>{row.risk}</TableCell>
+            <TableCell sx={cellStyle}>{row.review_date}</TableCell>
+            <TableCell sx={cellStyle}>
+              <IconButton />
+            </TableCell>
+          </TableRow>
+        ))}
+    </TableBody>
+  );
+
   return (
-    <TableContainer style={{}}>
+    <TableContainer>
       <Table
         sx={{
           border: "1px solid #EEEEEE",
@@ -37,61 +154,8 @@ function TableWithPlaceholder({ data = listOfVendors }) {
           },
         }}
       >
-        <TableHead
-          sx={{
-            backgroundColor: "#FAFAFA",
-          }}
-        >
-          <TableRow
-            sx={{
-              textTransform: "uppercase",
-              borderBottom: "1px solid #EEEEEE",
-            }}
-          >
-            {[
-              "name",
-              "type",
-              "assignee",
-              "status",
-              "risk",
-              "review date",
-              "",
-            ].map((cell, index) => (
-              <TableCell
-                style={{
-                  color: "#a1afc6",
-                  fontWeight: 400,
-                  paddingLeft: "16px",
-                }}
-                key={index}
-              >
-                {cell}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data &&
-            data.map((row, index) => (
-              <TableRow
-                key={index}
-                sx={{
-                  textTransform: "capitalize",
-                  borderBottom: "1px solid #EEEEEE",
-                }}
-              >
-                <TableCell sx={cellStyle}>{row.name}</TableCell>
-                <TableCell sx={cellStyle}>{row.type}</TableCell>
-                <TableCell sx={cellStyle}>{row.assignee}</TableCell>
-                <TableCell sx={cellStyle}>{row.status}</TableCell>
-                <TableCell sx={cellStyle}>{row.risk}</TableCell>
-                <TableCell sx={cellStyle}>{row.review_date}</TableCell>
-                <TableCell sx={cellStyle}>
-                  <IconButton />
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
+        {tableHeader}
+        {tableBody}
       </Table>
       {!data && (
         <div
@@ -109,14 +173,8 @@ function TableWithPlaceholder({ data = listOfVendors }) {
           <img src={Placeholder} alt="Placeholder" />
         </div>
       )}
-      <AddNewVendor
-        isOpen={isOpen}
-        handleChange={handleChange}
-        setIsOpen={() => setIsOpen(false)}
-        value={value}
-      />
     </TableContainer>
   );
-}
+};
 
 export default TableWithPlaceholder;
--- Clients/src/presentation/pages/Vendors/index.tsx
+++ Clients/src/presentation/pages/Vendors/index.tsx
@@ -1,5 +1,4 @@
 import { Button, Stack, Typography, useTheme } from "@mui/material";
-import { ReactComponent as Back } from "../../assets/icons/left-arrow-long.svg";
 import "./index.css";
 import TableWithPlaceholder from "../../components/Table/WithPlaceholder";
 import { useState } from "react";
@@ -22,40 +21,6 @@ const Vendors = () => {
   return (
     <div className="vendors-page">
       <Stack gap={theme.spacing(10)}>
-        <Stack
-          justifyContent={"space-between"}
-          display={"flex"}
-          flexDirection={"row"}
-          marginBottom={theme.spacing(6)}
-        >
-          <Button
-            disableRipple={
-              theme.components?.MuiButton?.defaultProps?.disableRipple
-            }
-            variant="contained"
-            color="inherit"
-            sx={{
-              width: 100,
-              height: 30,
-              gap: 6,
-              fontSize: 13,
-              boxShadow: "none",
-              backgroundColor: "#F5F5F5",
-              borderRadius: "4px",
-              border: "1px solid #EBEBEB",
-              alignItems: "center",
-              "&:hover": {
-                backgroundColor: "#F5F5F5",
-                boxShadow: "none",
-              },
-              textTransform: "capitalize",
-            }}
-            onClick={() => console.log("Back button")}
-          >
-            <Back />
-            Back
-          </Button>
-        </Stack>
         <Stack>
           <Typography fontSize={16} color="#1A1919" fontWeight={600}>
             Vendors list
@@ -97,34 +62,6 @@ const Vendors = () => {
           </Button>
         </Stack>
         <TableWithPlaceholder />
-        <Stack
-          sx={{
-            alignItems: "flex-end",
-          }}
-        >
-          <Button
-            disableRipple={
-              theme.components?.MuiButton?.defaultProps?.disableRipple
-            }
-            variant="contained"
-            sx={{
-              width: 150,
-              height: 34,
-              fontSize: 13,
-              textTransform: "capitalize",
-              backgroundColor: "#4C7DE7",
-              boxShadow: "none",
-              borderRadius: "4px",
-              border: "1px solid #175CD3",
-              "&:hover": {
-                boxShadow: "none",
-              },
-            }}
-            onClick={() => console.log("Save button")}
-          >
-            Save
-          </Button>
-        </Stack>
       </Stack>
       <AddNewVendor
         isOpen={isOpen}

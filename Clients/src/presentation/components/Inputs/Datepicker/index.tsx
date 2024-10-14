a3bf5464a075aa4e03c49be00e688a365cf139fd
Yevheniia Popova <popovaevg18@gmail.com>
2024-10-13 19:21:10 -0600

    Small fix

--- Clients/src/presentation/components/Inputs/Datepicker/index.tsx
+++ Clients/src/presentation/components/Inputs/Datepicker/index.tsx
@@ -3,7 +3,6 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker as MuiDatePicker } from "@mui/x-date-pickers/DatePicker";
 import "./index.css";
-import { Opacity } from "@mui/icons-material";
 
 interface DatePickerProps {
   label?: string;

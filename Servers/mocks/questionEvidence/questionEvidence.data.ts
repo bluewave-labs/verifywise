d3c256fc521ac6741e48a769214896c64aa29424
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-10-18 21:19:59 +0330

    Question evidence model added

---
+++ Servers/mocks/questionEvidence/questionEvidence.data.ts
@@ -0,0 +1,39 @@
+const question_evidence = [
+  {
+    id: 1,
+    question_id: 101,
+    section_id: 10,
+    assessment_review: "Review for question 101",
+    evidence: "Evidence for question 101",
+  },
+  {
+    id: 2,
+    question_id: 102,
+    section_id: 10,
+    assessment_review: "Review for question 102",
+    evidence: "Evidence for question 102",
+  },
+  {
+    id: 3,
+    question_id: 103,
+    section_id: 11,
+    assessment_review: "Review for question 103",
+    evidence: "Evidence for question 103",
+  },
+  {
+    id: 4,
+    question_id: 104,
+    section_id: 11,
+    assessment_review: "Review for question 104",
+    evidence: "Evidence for question 104",
+  },
+  {
+    id: 5,
+    question_id: 105,
+    section_id: 12,
+    assessment_review: "Review for question 105",
+    evidence: "Evidence for question 105",
+  },
+];
+
+export { question_evidence };

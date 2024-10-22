0da797eb90769466b3d5a804c48bc032d2d0e459
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-10-22 13:35:29 +0330

    Log mechanism in the frontend application layer

---
+++ Clients/src/application/tools/log.engine.ts
@@ -0,0 +1,44 @@
+import * as fs from "fs";
+import * as path from "path";
+
+interface LogProps {
+  type: "info" | "error" | "event";
+  message: string;
+  user: {
+    id: string;
+    email: string;
+    firstname: string;
+    lastname: string;
+  };
+  timestamp?: Date;
+}
+
+export function logEngine(props: LogProps) {
+  const { type, message, user, timestamp = new Date() } = props;
+
+  const logMessage = `LOG TYPE [${type.toUpperCase()}] | FOR THE USER WITH THE FOLLOWING DETAILS OF [ID: ${
+    user?.id ?? "N/A"
+  }, EMAIL: ${user?.email ?? "N/A"}, FIRSTNAME: ${
+    user?.firstname ?? "N/A"
+  }, LASTNAME: ${
+    user?.lastname ?? "N/A"
+  }] | MESSAGE: [${message}] | OCCURED AT [${timestamp.toISOString()}]`;
+
+  if (type === "info") {
+    console.info(logMessage);
+  } else if (type === "error") {
+    console.error(logMessage);
+  } else if (type === "event") {
+    console.log(logMessage);
+  }
+
+  const logFilePath = path.join(__dirname, `${type}.log`);
+
+  fs.appendFile(logFilePath, logMessage, (err) => {
+    if (err) {
+      console.error(
+        `Failed to write the log to the file ${type}.log in the path ${__dirname}`
+      );
+    }
+  });
+}

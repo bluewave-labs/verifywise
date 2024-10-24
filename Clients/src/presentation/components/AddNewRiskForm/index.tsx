0b46c31faf9ebe71ac88d73691458c160fb7f300
Yevheniia Popova <popovaevg18@gmail.com>
2024-10-23 18:22:02 -0600

    Added the popup template, created Risks tab fields

---
+++ Clients/src/presentation/components/AddNewRiskForm/index.tsx
@@ -0,0 +1,230 @@
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { Box, Divider, Stack, Tab, Typography, useTheme } from "@mui/material";
+import { FC, useState } from "react";
+import Select from "../Inputs/Select";
+import Field from "../Inputs/Field";
+
+const AddNewRiskForm: FC = () => {
+    const theme = useTheme();
+    const disableRipple = theme.components?.MuiButton?.defaultProps?.disableRipple;
+    const [value, setValue] = useState("risks");
+    const handleChange = (_: React.SyntheticEvent, newValue: string) => {
+      setValue(newValue);
+    };
+    const [values, setValues] = useState({
+        projectName: "Select project",
+        actionOwner: "Select owner",
+        aiLifecyclePhase: "Select phase",
+        riskDescription: "",
+        riskCategory: "Select category",
+        potentialImpact: "",
+        assessmentMapping: "Map assessment",
+        controlsMapping: "Map controls",
+        likelihood: "Select likelihood of risk to happen",
+        riskSeverity: "Select risk severity",
+        riskLevel: "Select risk level",
+        reviewNotes: ""
+
+    });
+    const handleOnChange = (field: string, value: string | number) => {
+        setValues((prevValues) => ({
+            ...prevValues,
+            [field]: value,
+        }));
+    };
+    const fieldStyle = {
+        backgroundColor: theme.palette.background.main,
+        "& input": {
+            padding: "0 14px"
+        }
+    }
+    const tabStyle = {
+        textTransform: "none",
+        fontWeight: 400,
+        alignItems: "flex-start",
+        justifyContent: "flex-end",
+        padding: "16px 0 7px",
+        minHeight: "20px"
+    };
+
+    const riskTabRender = () => {
+        return (
+            <Stack>
+                 <Select
+                    id="project-name-input"
+                    label="Project name"
+                    placeholder="Select project"
+                    value={values.projectName}
+                    onChange={(e) => handleOnChange("projectName", e.target.value)}
+                    items={[
+                        { _id: 1, name: "Some value 1" },
+                        { _id: 2, name: "Some value 2" },
+                        { _id: 3, name: "Some value 3" },
+                    ]}
+                    sx={{ width: "350px", backgroundColor: theme.palette.background.main }}
+                />
+                <Select
+                    id="action-owner-input"
+                    label="Action owner"
+                    placeholder="Select owner"
+                    value={values.actionOwner}
+                    onChange={(e) => handleOnChange("actionOwner", e.target.value)}
+                    items={[
+                        { _id: 1, name: "Some value 1" },
+                        { _id: 2, name: "Some value 2" },
+                        { _id: 3, name: "Some value 3" },
+                    ]}
+                    sx={{ width: "350px", backgroundColor: theme.palette.background.main }}
+                />
+                <Select
+                    id="ai-lifecycle-phase-input"
+                    label="AI lifecycle phase"
+                    placeholder="Select phase"
+                    value={values.aiLifecyclePhase}
+                    onChange={(e) => handleOnChange("aiLifecyclePhase", e.target.value)}
+                    items={[
+                        { _id: 1, name: "Some value 1" },
+                        { _id: 2, name: "Some value 2" },
+                        { _id: 3, name: "Some value 3" },
+                    ]}
+                    sx={{ width: "350px", backgroundColor: theme.palette.background.main }}
+                />
+                <Field 
+                    id="risk-description-input" 
+                    label="Risk description" 
+                    width={350}
+                    value={values.riskDescription}
+                    onChange={(e) => handleOnChange("riskDescription", e.target.value)}
+                    sx={fieldStyle} 
+                />
+                <Select
+                    id="risk-category-input"
+                    label="Risk category"
+                    placeholder="Select category"
+                    value={values.riskCategory}
+                    onChange={(e) => handleOnChange("riskCategory", e.target.value)}
+                    items={[
+                        { _id: 1, name: "Some value 1" },
+                        { _id: 2, name: "Some value 2" },
+                        { _id: 3, name: "Some value 3" },
+                    ]}
+                    sx={{ width: "350px", backgroundColor: theme.palette.background.main }}
+                />
+                <Field 
+                    id="potential-impact-input" 
+                    label="Potential Impact" 
+                    type="description" 
+                    value={values.potentialImpact}
+                    onChange={(e) => handleOnChange("potentialImpact", e.target.value)}
+                    sx={{ height: 101, backgroundColor: theme.palette.background.main }} 
+                />
+                <Select
+                    id="assessment-mapping-input"
+                    label="Assessment mapping"
+                    placeholder="Map assessment"
+                    value={values.assessmentMapping}
+                    onChange={(e) => handleOnChange("assessmentMapping", e.target.value)}
+                    items={[
+                        { _id: 1, name: "Some value 1" },
+                        { _id: 2, name: "Some value 2" },
+                        { _id: 3, name: "Some value 3" },
+                    ]}
+                    sx={{ width: "350px", backgroundColor: theme.palette.background.main }}
+                />
+                <Select
+                    id="controls-mapping-input"
+                    label="Controls mapping"
+                    placeholder="Map controls"
+                    value={values.controlsMapping}
+                    onChange={(e) => handleOnChange("controlsMapping", e.target.value)}
+                    items={[
+                        { _id: 1, name: "Some value 1" },
+                        { _id: 2, name: "Some value 2" },
+                        { _id: 3, name: "Some value 3" },
+                    ]}
+                    sx={{ width: "350px", backgroundColor: theme.palette.background.main }}
+                />
+                <Divider />
+                <Typography>Calculate risk level</Typography>
+                <Typography>The Risk Level is calculated by multiplying the Likelihood and Severity scores. By assigning these scores, the risk level will be determined based on your inputs.</Typography>
+                <Select
+                    id="likelihood-input"
+                    label="Likelihood"
+                    placeholder="Select likelihood of risk to happen"
+                    value={values.likelihood}
+                    onChange={(e) => handleOnChange("likelihood", e.target.value)}
+                    items={[
+                        { _id: 1, name: "Some value 1" },
+                        { _id: 2, name: "Some value 2" },
+                        { _id: 3, name: "Some value 3" },
+                    ]}
+                    sx={{ width: "350px", backgroundColor: theme.palette.background.main }}
+                />
+                <Select
+                    id="risk-severity-input"
+                    label="Risk severity"
+                    placeholder="Select risk severity"
+                    value={values.riskSeverity}
+                    onChange={(e) => handleOnChange("riskSeverity", e.target.value)}
+                    items={[
+                        { _id: 1, name: "Some value 1" },
+                        { _id: 2, name: "Some value 2" },
+                        { _id: 3, name: "Some value 3" },
+                    ]}
+                    sx={{ width: "350px", backgroundColor: theme.palette.background.main }}
+                />
+                <Select
+                    id="risk-level-input"
+                    label="Risk level (auto-calculated)"
+                    placeholder="Select risk level"
+                    value={values.riskLevel}
+                    onChange={(e) => handleOnChange("riskLevel", e.target.value)}
+                    items={[
+                        { _id: 1, name: "Some value 1" },
+                        { _id: 2, name: "Some value 2" },
+                        { _id: 3, name: "Some value 3" },
+                    ]}
+                    sx={{ width: "350px", backgroundColor: theme.palette.background.main }}
+                />
+                <Field 
+                    id="review-notes-input" 
+                    label="Review notes" 
+                    type="description" 
+                    value={values.reviewNotes}
+                    onChange={(e) => handleOnChange("reviewNotes", e.target.value)}
+                    sx={{ height: 101, backgroundColor: theme.palette.background.main }} 
+                />
+            </Stack>
+        )
+    }
+
+    return (
+        <Stack
+            component="form"
+            noValidate
+            sx={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", columnGap: "26px", rowGap: "17px" }}
+        >
+            <TabContext value={value}>
+                    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                        <TabList onChange={handleChange} aria-label="Add new risk tabs" 
+                            sx={{ minHeight: "20px", 
+                                "& .MuiTabs-flexContainer": { columnGap: "34px" } }}
+                        >
+                            <Tab label="Risks" value="risks" sx={tabStyle} disableRipple={disableRipple} />
+                            <Tab label="Mitigation" value="mitigation" sx={tabStyle} disableRipple={disableRipple} />
+                        </TabList>
+                    </Box>
+                    <TabPanel value="risks" sx={{ p: "24px 0 0" }}>
+
+                    </TabPanel>
+                    <TabPanel value="mitigation" sx={{ p: "24px 0 0" }}>
+                        
+                    </TabPanel>
+                </TabContext>
+        </Stack>
+    )
+}
+
+export default AddNewRiskForm;

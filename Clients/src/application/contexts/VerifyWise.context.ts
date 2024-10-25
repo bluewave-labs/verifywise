3a288e49ed224962aa64156fa20093c9b064999d
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-10-25 13:00:26 +0330

    Creating VerifyWise Context

---
+++ Clients/src/application/contexts/VerifyWise.context.ts
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+export const VerifyWiseContext = createContext({
+  // States for tthe UI
+  // States for Authentication and User Profile
+  // States for the whole dashboard
+  // Dashboard
+  // Projects
+  // Compliance Tracker
+  // Assessment Tracker
+  // Vendors
+  // File Manager
+  // Settings
+});

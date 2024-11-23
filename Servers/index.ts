4b86cb5bc92e3ba63c41c01f4fc944c82e6fc008
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-11-23 09:33:36 +0330

    cors

--- Servers/index.ts
+++ Servers/index.ts
@@ -26,7 +26,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/vendorRisks", vendorRiskRoutes);

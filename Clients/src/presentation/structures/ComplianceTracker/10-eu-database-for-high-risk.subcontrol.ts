d0889fce30bcaafea9839d9362bc6429dca612ff
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-11-30 11:36:50 +0330

    EU database Controls and subcontrols added

---
+++ Clients/src/presentation/structures/ComplianceTracker/10-eu-database-for-high-risk.subcontrol.ts
@@ -0,0 +1,70 @@
+export const EUdatabaseForHighRiskAI = {
+  id: 10,
+  controls: [
+    {
+      id: 1,
+      title: "Registration Activity Records Maintenance",
+      description:
+        "Maintain comprehensive records of high-risk AI systems in the EU database, ensuring compliance with documentation and responsiveness to authorities.",
+      subControls: [
+        {
+          id: 1,
+          title:
+            "We engage with notified bodies or conduct internal conformity assessments.",
+          description:
+            "Collaboration with notified bodies or conducting internal reviews ensures rigorous evaluation of AI system compliance.",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Registration Activity Records Maintenance",
+      description:
+        "Implement and document monitoring systems to track AI system performance and address risks post-deployment.",
+      subControls: [
+        {
+          id: 1,
+          title:
+            "We establish processes to respond to national authority requests.",
+          description:
+            "Well-defined processes enable efficient and accurate responses to regulatory inquiries or audits.",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Registration Activity Records Maintenance",
+      description:
+        "Maintain up-to-date registration information and comprehensive conformity documentation.",
+      subControls: [
+        {
+          id: 1,
+          title: "We maintain thorough documentation of AI system conformity.",
+          description:
+            "Comprehensive conformity documentation demonstrates adherence to legal standards and supports regulatory reporting.",
+        },
+        {
+          id: 2,
+          title: "We keep records of registration and any subsequent updates.",
+          description:
+            "Detailed records of registration activities provide transparency and facilitate compliance verification.",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "EU Database Data Entry Timeliness",
+      description:
+        "Maintain up-to-date registration information and comprehensive conformity documentation.",
+      subControls: [
+        {
+          id: 1,
+          title:
+            "We ensure timely and accurate data entry into the EU database.",
+          description:
+            "Ensuring timely updates maintains regulatory compliance and fosters trust in system integrity and monitoring processes.",
+        },
+      ],
+    },
+  ],
+};

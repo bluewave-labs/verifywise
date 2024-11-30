d253e548060e1f125529b20b5ef7ac2745976e88
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-11-30 10:48:40 +0330

    Transparency obligations for providers Controls and subcontrols added

---
+++ Clients/src/presentation/structures/ComplianceTracker/08-transparency-obligations-for-providers.subcontrol.ts
@@ -0,0 +1,71 @@
+export const TransparencyObligationsForProviders = {
+  id: 7,
+  controls: [
+    {
+      id: 1,
+      title: "User Notification of AI System Use",
+      description:
+        "Ensure clear communication that users are interacting with AI systems and provide comprehensive information about AI system functionalities and impacts.",
+      subControls: [
+        {
+          id: 1,
+          title:
+            "We design AI systems to clearly indicate user interaction with AI.",
+          description:
+            "Clear indicators help users understand when they are interacting with AI, promoting transparency and trust.",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Clear AI Indication for Users",
+      description:
+        "Ensure AI indications are clear and understandable for reasonably informed users.",
+      subControls: [
+        {
+          id: 1,
+          title: "We inform users when they are subject to AI system usage.",
+          description:
+            "Transparent communication ensures users are aware of and consent to AI system interactions affecting them.",
+        },
+        {
+          id: 2,
+          title:
+            "We ensure AI indications are clear and understandable for reasonably informed users.",
+          description:
+            "Providing clear, simple AI indications allows users to make informed decisions and understand system limitations.",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "AI System Scope and Impact Definition",
+      description:
+        "Define and document AI system scope, goals, methods, and potential impacts.",
+      subControls: [
+        {
+          id: 1,
+          title:
+            "We define and document AI system scope, goals, methods, and potential impacts.",
+          description:
+            "Comprehensive documentation helps align AI deployment with intended goals and prepares for potential risks.",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "AI System Scope and Impact Definition",
+      description:
+        "Maintain accurate records of AI system activities, including modifications and third-party involvements.",
+      subControls: [
+        {
+          id: 1,
+          title:
+            "We maintain accurate records of AI system activities, modifications, and third-party involvements.",
+          description:
+            "Accurate records ensure accountability and support audits, troubleshooting, and regulatory compliance.",
+        },
+      ],
+    },
+  ],
+};

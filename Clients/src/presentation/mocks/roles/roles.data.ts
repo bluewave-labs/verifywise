b800e2fc31a41967376b89113ea31a5664713054
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-12-08 21:08:43 +0330

    npm run build runs successfully

--- Clients/src/presentation/mocks/roles/roles.data.ts
+++ Clients/src/presentation/mocks/roles/roles.data.ts
@@ -1,23 +1,23 @@
 // Role table mock data
-const roles = [
+export const roles = [
   {
     id: 1,
     name: "Admin",
-    description: "Administrator with full access to the system."
+    description: "Administrator with full access to the system.",
   },
   {
     id: 2,
     name: "Reviewer",
-    description: "Reviewer with access to review compliance and reports."
+    description: "Reviewer with access to review compliance and reports.",
   },
   {
     id: 3,
     name: "Editor",
-    description: "Editor with permission to modify and update project details."
+    description: "Editor with permission to modify and update project details.",
   },
   {
     id: 4,
     name: "Auditor",
-    description: "Auditor with access to compliance and security audits."
-  }
+    description: "Auditor with access to compliance and security audits.",
+  },
 ];

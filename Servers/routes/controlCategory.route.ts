e3a9d905fc75eaaf7cb99c2a5f2b205f95351f86
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-12-11 11:27:51 +0330

    control category routes are set

--- Servers/routes/controlCategory.route.ts
+++ Servers/routes/controlCategory.route.ts
@@ -2,18 +2,25 @@ import express from "express";
 const router = express.Router();
 
 import authenticateJWT from "../middleware/auth.middleware";
+import {
+  createControlCategory,
+  deleteControlCategoryById,
+  getAllControlCategories,
+  getControlCategoryById,
+  updateControlCategoryById,
+} from "../controllers/controlCategory.ctrl";
 
 // GET requests
-router.get("/" /*authenticateJWT, */);
-router.get("/:id" /*authenticateJWT, */);
+router.get("/" /*authenticateJWT, */, getAllControlCategories);
+router.get("/:id" /*authenticateJWT, */, getControlCategoryById);
 
 // POSTs
-router.post("/" /*authenticateJWT, */);
+router.post("/" /*authenticateJWT, */, createControlCategory);
 
 // PUTs
-router.put("/:id" /*authenticateJWT, */);
+router.put("/:id" /*authenticateJWT, */, updateControlCategoryById);
 
 // DELETEs
-router.delete("/:id" /*authenticateJWT, */);
+router.delete("/:id" /*authenticateJWT, */, deleteControlCategoryById);
 
 export default router;

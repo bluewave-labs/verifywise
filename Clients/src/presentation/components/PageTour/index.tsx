8233cec51414a03c4b436e58436b70d514cdad9b
salmanja <71104867+salmanja@users.noreply.github.com>
2025-01-02 00:01:23 -0500

    init global tour component

--- Clients/src/presentation/components/PageTour/index.tsx
+++ Clients/src/presentation/components/PageTour/index.tsx
@@ -1,1 +1,43 @@
-//joyride global component to show the tour
+import Joyride, {Step} from 'react-joyride';
+import React, {useEffect, useState} from 'react';
+
+interface PageTourProps {
+    steps: Step[];
+    onFinish?:()=>void;
+}
+
+const PageTour: React.FC<PageTourProps> =({steps, onFinish}) => {
+    const [run, setRun] = useState(false);
+
+    useEffect (()=>{
+        //always check if tour was seen first before running it
+         const hasSeenTour = localStorage.getItem("hasSeenTour");
+         if(!hasSeenTour){
+            setRun(true);
+         }
+    }, [])
+
+    const handleCallback = (data:any)=>{
+        const {status} = data;
+        if (status === 'finished' || status === 'skipper'){
+            localStorage.setItem("hasSeenTour", "true");
+            setRun(false);
+            if (onFinish){
+                onFinish();
+            }
+        }
+    }
+
+    return (
+        <Joyride 
+        steps={steps}
+        run={run}
+        continuous
+        hideCloseButton
+        showProgress
+        showSkipButton
+        callback={handleCallback}
+        />
+    )
+}
+export default PageTour;

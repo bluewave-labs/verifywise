84ca7f8e832ad7cbc3d2cea3fd38925f6751ec8e
Mel <80449420+melpsh@users.noreply.github.com>
2025-01-05 18:35:14 -0800

    updated the UI

--- Clients/src/presentation/components/Modals/InviteUser/index.tsx
+++ Clients/src/presentation/components/Modals/InviteUser/index.tsx
@@ -14,151 +14,145 @@
  */
 
 import {
-    Button,
-    MenuItem,
-    Modal,
-    Select,
-    Stack,
-    TextField,
-    Typography,
-    useTheme,
-  } from "@mui/material";
-  import React, { useState } from "react";
-  
-  interface InviteUserModalProps {
-    isOpen: boolean;
-    setIsOpen: (isOpen: boolean) => void;
-    onSendInvite: (email: string, role: string) => void;
-  }
-  
-  const InviteUserModal: React.FC<InviteUserModalProps> = ({
-    isOpen,
-    setIsOpen,
-    onSendInvite,
-  }) => {
-    const theme = useTheme();
-  
-    const [email, setEmail] = useState<string>("");
-    const [role, setRole] = useState<string>("");
-  
-    const handleSendInvite = () => {
-      onSendInvite(email, role);
-      setIsOpen(false);
-    };
-  
-    return (
-      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-        <Stack
-          gap={theme.spacing(2)}
-          color={theme.palette.text.secondary}
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 450,
-            bgcolor: theme.palette.background.paper,
-            border: 1,
-            borderColor: theme.palette.border.dark,
-            borderRadius: theme.shape.borderRadius,
-            boxShadow: 24,
-            p: theme.spacing(4),
-            padding: theme.spacing(12),
-            "&:focus": {
-              outline: "none",
-            },
-          }}
-        >
-          <Typography id="modal-invite-user" fontSize={16} fontWeight={600}>
-            Invite new team member
-          </Typography>
-          <Typography
-            id="invite-user-instructions"
-            fontSize={13}
-            textAlign={"justify"}
-            paddingBottom={theme.spacing(8)}
-          >
-            When you add a new team member, they will get access to all monitors.
-          </Typography>
-          <TextField
-            label="Email"
-            type="email"
-            fullWidth
-            variant="outlined"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            sx={{
-              marginBottom: theme.spacing(4),
-            }}
-          />
-          <Select
-            fullWidth
-            value={role}
-            onChange={(e) => setRole(e.target.value)}
-            displayEmpty
-            sx={{ mt: theme.spacing(2) }}
-          >
-            <MenuItem value="" disabled>
-              Select Role
-            </MenuItem>
-            <MenuItem value="administrator">Administrator</MenuItem>
-            <MenuItem value="reviewer">Reviewer</MenuItem>
-            <MenuItem value="editor">Editor</MenuItem>
-          </Select>
-          <Stack
-            direction="row"
-            gap={theme.spacing(4)}
-            mt={theme.spacing(4)}
-            justifyContent="flex-end"
-            paddingBottom={theme.spacing(4)}
-          >
-            <Button
-              disableRipple
-              disableFocusRipple
-              disableTouchRipple
-              variant="text"
-              color="inherit"
-              onClick={() => setIsOpen(false)}
-              sx={{
-                width: 100,
-                textTransform: "capitalize",
-                fontSize: 13,
-                borderRadius: "4px",
-                "&:hover": {
-                  boxShadow: "none",
-                  backgroundColor: "transparent",
-                },
-              }}
-            >
-              Cancel
-            </Button>
-            <Button
-              disableRipple
-              disableFocusRipple
-              disableTouchRipple
-              variant="contained"
-              color="primary"
-              disabled={!email || !role}
-              sx={{
-                width: 140,
-                textTransform: "capitalize",
-                fontSize: 13,
-                boxShadow: "none",
-                borderRadius: "4px",
-                "&:hover": {
-                  boxShadow: "none",
-                },
-              }}
-              onClick={handleSendInvite}
-            >
-              Send Invite
-            </Button>
-          </Stack>
-        </Stack>
-      </Modal>
-    );
+  Button,
+  MenuItem,
+  Modal,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import React, { useState } from "react";
+import Field from "../../Inputs/Field";
+import Select from "../../Inputs/Select";
+
+interface InviteUserModalProps {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  onSendInvite: (email: string, role: string) => void;
+}
+
+const InviteUserModal: React.FC<InviteUserModalProps> = ({
+  isOpen,
+  setIsOpen,
+  onSendInvite,
+}) => {
+  const theme = useTheme();
+
+  const [email, setEmail] = useState<string>("");
+  const [role, setRole] = useState<string>("");
+
+  const handleSendInvite = () => {
+    onSendInvite(email, role);
+    setIsOpen(false);
   };
-  
-  export default InviteUserModal;
-  
+
+  return (
+    <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+      <Stack
+        gap={theme.spacing(2)}
+        color={theme.palette.text.secondary}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 450,
+          bgcolor: theme.palette.background.paper,
+          border: 1,
+          borderColor: theme.palette.border.dark,
+          borderRadius: theme.shape.borderRadius,
+          boxShadow: 24,
+          p: theme.spacing(4),
+          padding: theme.spacing(12),
+          "&:focus": {
+            outline: "none",
+          },
+        }}
+      >
+        <Typography id="modal-invite-user" fontSize={16} fontWeight={600}>
+          Invite new team member
+        </Typography>
+        <Typography
+          id="invite-user-instructions"
+          fontSize={13}
+          textAlign={"justify"}
+          paddingBottom={theme.spacing(8)}
+        >
+          When you add a new team member, they will get access to all monitors.
+        </Typography>
+        <Field
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          isRequired
+          sx={{
+            marginBottom: theme.spacing(4),
+          }}
+        />
+        <Select
+          id="role-select"
+          value={role}
+          onChange={(e) => setRole(e.target.value as string)}
+          items={[
+            { _id: "administrator", name: "Administrator" },
+            { _id: "reviewer", name: "Reviewer" },
+            { _id: "editor", name: "Editor" },
+          ]}
+          sx={{ mt: theme.spacing(2) }}
+        />
+        <Stack
+          direction="row"
+          gap={theme.spacing(4)}
+          mt={theme.spacing(4)}
+          justifyContent="flex-end"
+          paddingBottom={theme.spacing(4)}
+        >
+          <Button
+            disableRipple
+            disableFocusRipple
+            disableTouchRipple
+            variant="text"
+            color="inherit"
+            onClick={() => setIsOpen(false)}
+            sx={{
+              width: 100,
+              textTransform: "capitalize",
+              fontSize: 13,
+              borderRadius: "4px",
+              "&:hover": {
+                boxShadow: "none",
+                backgroundColor: "transparent",
+              },
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            disableRipple
+            disableFocusRipple
+            disableTouchRipple
+            variant="contained"
+            color="primary"
+            disabled={!email || !role}
+            sx={{
+              width: 140,
+              textTransform: "capitalize",
+              fontSize: 13,
+              boxShadow: "none",
+              borderRadius: "4px",
+              "&:hover": {
+                boxShadow: "none",
+              },
+            }}
+            onClick={handleSendInvite}
+          >
+            Send Invite
+          </Button>
+        </Stack>
+      </Stack>
+    </Modal>
+  );
+};
+
+export default InviteUserModal;

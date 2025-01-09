2aae9324a438418410f7520edfe4473696bbf836
Charu Rajput <charurajput89@gmail.com>
2025-01-09 12:05:23 -0500

    popup backgrounds unified

--- Clients/src/presentation/vw-v2-components/Dialogs/DualButtonModal/index.tsx
+++ Clients/src/presentation/vw-v2-components/Dialogs/DualButtonModal/index.tsx
@@ -30,6 +30,18 @@ const DualButtonModal: React.FC<DualButtonModalProps> = ({
   proceedButtonVariant,
 }) => {
   return (
+    <>
+    <Stack
+        sx={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          zIndex: 1299,
+        }}
+      />
     <Stack
       className="dual-btn-modal"
       sx={{
@@ -72,6 +84,7 @@ const DualButtonModal: React.FC<DualButtonModalProps> = ({
         />
       </Stack>
     </Stack>
+    </>
   );
 };
 

e5defb010ceb10aba260d96ec764899cc8dd4dd0
salmanja <71104867+salmanja@users.noreply.github.com>
2025-01-19 02:41:10 -0500

    removed the disabled tabs and instead created empty state

--- Clients/src/presentation/pages/ProjectView/index.tsx
+++ Clients/src/presentation/pages/ProjectView/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Tab, Typography, useTheme } from "@mui/material";
+import { Box, Button, Stack, Tab, Typography, useTheme } from "@mui/material";
 import projectOverviewData from "../../mocks/projects/project-overview.data";
 import React from "react";
 import TabContext from "@mui/lab/TabContext";
@@ -9,8 +9,10 @@ import RisksView from "./RisksView";
 import projectRisksData from "../../mocks/projects/project-risks.data";
 import vendorRisksData from "../../mocks/projects/project-vendor-risks.data";
 import ProjectSettings from "./ProjectSettings";
+import emptyStateImg from "../../assets/imgs/empty-state.svg";
 
 const ProjectView = ({ project = projectOverviewData }) => {
+  //project will be { } for testing 
   const { projectTitle, projectRisks, vendorRisks } = project;
   const theme = useTheme();
   const disableRipple =
@@ -40,69 +42,130 @@ const ProjectView = ({ project = projectOverviewData }) => {
 
   return (
     <Stack>
-      <Typography
-        sx={{ color: "#1A1919", fontWeight: 600, mb: "6px", fontSize: 16 }}
-      >
-        {projectTitle} project overview
-      </Typography>
-      <Typography
-        sx={{
-          fontSize: theme.typography.fontSize,
-          color: theme.palette.text.secondary,
-        }}
-      >
-        This project includes all the governance process status of the Chatbot
-        AI project.
-      </Typography>
-      <Stack
-        sx={{ minWidth: "968px", overflowX: "auto", whiteSpace: "nowrap" }}
-      >
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <TabList
-              onChange={handleChange}
-              aria-label="project view tabs"
-              sx={{
-                minHeight: "20px",
-                "& .MuiTabs-flexContainer": { columnGap: "34px" },
+      {noProject ? (
+        //no project found template
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+            mt: "79px",
+            mx: "auto",
+            p: 10,
+            border: `1px solid ${theme.palette.divider}`,
+            borderRadius: "4px",
+            width: { xs: "90%", sm: "90%", md: "1056px" },
+            maxWidth: "100%",
+            height: { xs: "100%", md: "418px" },
+          }}
+        >
+          {/* empty state image */}
+          <Box sx={{ mb: 8 }}>
+            <img
+              src={emptyStateImg}
+              alt="No project found"
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+                objectFit: "contain",
               }}
-            >
-              <Tab
-                label="Overview"
-                value="overview"
-                sx={tabStyle}
-                disableRipple={disableRipple}
-                disabled={noProject}
-              />
-              <Tab
-                label="Project risks"
-                value="project-risks"
-                sx={tabStyle}
-                disableRipple={disableRipple}
-                disabled={noProject}
-              />
-              <Tab
-                label="Vendor risks"
-                value="vendor-risks"
-                sx={tabStyle}
-                disableRipple={disableRipple}
-                disabled={noProject}
-              />
-              <Tab
-                label="Settings"
-                value="settings"
-                sx={tabStyle}
-                disableRipple={disableRipple}
-                disabled={noProject}
-              />
-            </TabList>
+            />
           </Box>
-          {/* overview panel */}
-          <TabPanel value="overview" sx={{ p: "32px 0 0" }}>
-            <Overview mocProject={project} />
-          </TabPanel>
-          {!noProject && (
-            <>
+
+          {/* Subtitle */}
+          <Typography
+            variant="body2"
+            sx={{
+              mb: 12,
+              color: theme.palette.text.secondary,
+              fontSize: "13px",
+            }}
+          >
+            No projects found. Create a new project to start with.
+          </Typography>
+          {/* new project button */}
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              console.log("redirect to create new project");
+              //add naviagtion to new project
+            }}
+            sx={{
+              textTransform: "none",
+              borderRadius: "4px",
+              py: 1.5,
+              backgroundColor: "#4C7DE7",
+              fontSize: "13px",
+              padding: "10px 16px",
+              width: "119px",
+              height: "34px",
+            }}
+          >
+            New Project
+          </Button>
+        </Box>
+      ) : (
+        <Stack>
+          <Typography
+            sx={{ color: "#1A1919", fontWeight: 600, mb: "6px", fontSize: 16 }}
+          >
+            {projectTitle} project overview
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: theme.typography.fontSize,
+              color: theme.palette.text.secondary,
+            }}
+          >
+            This project includes all the governance process status of the
+            Chatbot AI project.
+          </Typography>
+          <Stack
+            sx={{ minWidth: "968px", overflowX: "auto", whiteSpace: "nowrap" }}
+          >
+            <TabContext value={value}>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <TabList
+                  onChange={handleChange}
+                  aria-label="project view tabs"
+                  sx={{
+                    minHeight: "20px",
+                    "& .MuiTabs-flexContainer": { columnGap: "34px" },
+                  }}
+                >
+                  <Tab
+                    label="Overview"
+                    value="overview"
+                    sx={tabStyle}
+                    disableRipple={disableRipple}
+                  />
+                  <Tab
+                    label="Project risks"
+                    value="project-risks"
+                    sx={tabStyle}
+                    disableRipple={disableRipple}
+                  />
+                  <Tab
+                    label="Vendor risks"
+                    value="vendor-risks"
+                    sx={tabStyle}
+                    disableRipple={disableRipple}
+                  />
+                  <Tab
+                    label="Settings"
+                    value="settings"
+                    sx={tabStyle}
+                    disableRipple={disableRipple}
+                  />
+                </TabList>
+              </Box>
+              {/* overview panel */}
+              <TabPanel value="overview" sx={{ p: "32px 0 0" }}>
+                <Overview mocProject={project} />
+              </TabPanel>
               <TabPanel value="project-risks" sx={{ p: "32px 0 0" }}>
                 <RisksView
                   risksSummary={projectRisks}
@@ -120,10 +183,10 @@ const ProjectView = ({ project = projectOverviewData }) => {
               <TabPanel value="settings" sx={{ p: "32px 0 0" }}>
                 <ProjectSettings setTabValue={setValue} />
               </TabPanel>
-            </>
-          )}
-        </TabContext>
-      </Stack>
+            </TabContext>
+          </Stack>
+        </Stack>
+      )}
     </Stack>
   );
 };

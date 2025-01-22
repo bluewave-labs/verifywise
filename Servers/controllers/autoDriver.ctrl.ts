ee6fa1d391cde9daf44397b6087a8a34a1aba96e
Harsh <pharsh4585@gmail.com>
2025-01-21 20:37:48 -0500

    added endpoint to delete the mock data from the db

--- Servers/controllers/autoDriver.ctrl.ts
+++ Servers/controllers/autoDriver.ctrl.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from "express";
-import { insertMockData } from "../driver/autoDriver.driver";
+import { deleteMockData, insertMockData } from "../driver/autoDriver.driver";
 import { STATUS_CODE } from "../utils/statusCode.utils";
 
 export async function postAutoDriver(req: Request, res: Response) {
   try {
     insertMockData()
     res.status(201).json(STATUS_CODE[201]("Mock data inserted"))
+  } catch(error) {
+    return res.status(500).json(STATUS_CODE[500]((error as Error).message));
+  }
+}
+
+export async function deleteAutoDriver(req: Request, res: Response) {
+  try {
+    deleteMockData()
+    res.status(200).json(STATUS_CODE[200]("Mock data deleted"))
   } catch(error) {
     return res.status(500).json(STATUS_CODE[500]((error as Error).message));
   }

f2f8e77df778f37559d9a10a87783803327fa481
salmanja <71104867+salmanja@users.noreply.github.com>
2025-01-23 13:39:25 -0500

    fixed file size dsiplay

--- Clients/src/presentation/components/FileUpload/index.tsx
+++ Clients/src/presentation/components/FileUpload/index.tsx
@@ -74,6 +74,8 @@ const FileUploadComponent: React.FC<FileUploadProps> = ({
       return;
     }
 
+    const fileSizeInMB = file.size ? (file.size / (1024 * 1024)).toFixed(2) : "0.00";
+
     // Prevent duplicate files
     setUploadedFiles((prevFiles) => {
       const fileExists = prevFiles.some((f) => f.id === file.id);
@@ -86,6 +88,7 @@ const FileUploadComponent: React.FC<FileUploadProps> = ({
         {
           id: file.id,
           name: file.name,
+          size: `${fileSizeInMB} MB`,
           data: file.data,
         },
       ];

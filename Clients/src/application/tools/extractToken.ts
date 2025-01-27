9585b6c7468fb3bab209d9121afb968fea3cdc79
eem <eimon1150@gmail.com>
2025-01-27 13:56:42 -0600

    Add user token extract function

---
+++ Clients/src/application/tools/extractToken.ts
@@ -0,0 +1,25 @@
+export const extractUserToken = (token: string) => {
+    if (!token) {
+        console.error("Token is missing!");
+        return null;
+    }
+
+    try {        
+        const parts = token.split(".");
+        if (parts.length !== 3) {
+            console.error("Invalid token format");
+            return null;
+        }
+
+        const payloadBase64 = parts[1];
+        const payloadJson = atob(payloadBase64); 
+        const payload = JSON.parse(payloadJson); 
+        
+        const { id, email } = payload;
+
+        return { id, email };
+    } catch (error) {
+        console.error("Failed to decode token", error);
+        return null;
+    }
+}

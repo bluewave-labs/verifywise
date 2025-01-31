8da5196b977553005c5efd3dfafd92fc28e3f585
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-01-31 13:49:00 +0330

    Replacing userId with dynamic value

--- Clients/src/application/tools/extractToken.ts
+++ Clients/src/application/tools/extractToken.ts
@@ -1,25 +1,27 @@
-export const extractUserToken = (token: string) => {
-    if (!token) {
-        console.error("Token is missing!");
-        return null;
+export const extractUserToken = (
+  token: string
+): { id: any; email: any } | null => {
+  if (!token) {
+    console.error("Token is missing!");
+    return null;
+  }
+
+  try {
+    const parts = token.split(".");
+    if (parts.length !== 3) {
+      console.error("Invalid token format");
+      return null;
     }
 
-    try {        
-        const parts = token.split(".");
-        if (parts.length !== 3) {
-            console.error("Invalid token format");
-            return null;
-        }
+    const payloadBase64 = parts[1];
+    const payloadJson = atob(payloadBase64);
+    const payload = JSON.parse(payloadJson);
 
-        const payloadBase64 = parts[1];
-        const payloadJson = atob(payloadBase64); 
-        const payload = JSON.parse(payloadJson); 
-        
-        const { id, email } = payload;
+    const { id, email } = payload;
 
-        return { id, email };
-    } catch (error) {
-        console.error("Failed to decode token", error);
-        return null;
-    }
-}
+    return { id, email };
+  } catch (error) {
+    console.error("Failed to decode token", error);
+    return null;
+  }
+};

732184977fde40be131df584e565c4cf7d91af16
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-02-02 20:04:33 +0330

    Reset password added

--- Servers/routes/vwmailer.route.ts
+++ Servers/routes/vwmailer.route.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/invite", async (req, res) => {
   const { to, name, link } = req.body;
 
   if (!to || !name || !link) {
@@ -28,6 +28,40 @@ router.post("/", async (req, res) => {
       to,
       "Account Creation",
       "Please use the link to create your account.",
+      template,
+      data
+    );
+
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: "Failed to send email" });
+  }
+});
+
+router.post("/reset-password", async (req, res) => {
+  const { to, name, email, url } = req.body;
+
+  if (!to || !name || !email || !url) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  try {
+    // Read the MJML template file
+    const templatePath = path.resolve(
+      __dirname,
+      "../templates/password-reset-email.mjml"
+    );
+    const template = fs.readFileSync(templatePath, "utf8");
+
+    // Data to be replaced in the template
+    const data = { name, email, url };
+
+    // Send the email
+    await sendEmail(
+      to,
+      "Password Reset Request",
+      "Please use the link to reset your password.",
       template,
       data
     );

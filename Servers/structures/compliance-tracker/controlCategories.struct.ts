dceb772c9d40f00cb07837b91a5c6330c0d99ecf
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-02-10 17:57:37 +0330

    control category structure added

---
+++ Servers/structures/compliance-tracker/controlCategories.struct.ts
@@ -0,0 +1,47 @@
+export const ControlCategories = [
+  { order_no: 1, title: "AI literacy" },
+  {
+    order_no: 2,
+    title: "Transparency and provision of information to deployers",
+  },
+  { order_no: 3, title: "Human oversight" },
+  {
+    order_no: 4,
+    title: "Corrective actions and duty of information",
+  },
+  {
+    order_no: 5,
+    title: "Responsibilities along the AI value chain",
+  },
+  {
+    order_no: 6,
+    title: "Obligations of deployers of high-risk AI systems",
+  },
+  {
+    order_no: 7,
+    title: "Fundamental rights impact assessments for high-risk AI systems",
+  },
+  {
+    order_no: 8,
+    title:
+      "Transparency obligations for providers and users of certain AI systems",
+  },
+  { order_no: 9, title: "Registration" },
+  {
+    order_no: 10,
+    title: "EU database for high-risk AI systems listed in Annex III",
+  },
+  {
+    order_no: 11,
+    title:
+      "Post-market monitoring by providers and post-market monitoring plan for high-risk AI systems",
+  },
+  {
+    order_no: 12,
+    title: "Reporting of serious incidents",
+  },
+  {
+    order_no: 13,
+    title: "General-purpose AI models",
+  },
+];

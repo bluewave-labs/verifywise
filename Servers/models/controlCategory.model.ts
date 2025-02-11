19fbb1432817b482112bbef305a0ad3e27b628ce
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-02-11 19:37:09 +0330

    new control_category model added, commented

--- Servers/models/controlCategory.model.ts
+++ Servers/models/controlCategory.model.ts
@@ -1,3 +1,16 @@
+/*
+
+This is the new control category model(Schema) and will be replaced with the new one.
+Please align other files with this
+
+export type control_category = {
+  id?: number; //automatically created by database
+  project_id: number; // FK to the project table
+  title: string; // gets assigned from the structure
+  order_no: number; // gets assigned from the structure
+}
+*/
+
 export type ControlCategory = {
   id?: number;
   projectId: number;

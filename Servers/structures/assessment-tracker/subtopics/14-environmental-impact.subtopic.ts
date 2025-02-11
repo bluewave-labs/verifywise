206f39f83a0f0373681e6dc4b2988b2bb925ea3a
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-02-11 09:11:53 +0330

    EnvironmentalImpact structure

---
+++ Servers/structures/assessment-tracker/subtopics/14-environmental-impact.subtopic.ts
@@ -0,0 +1,34 @@
+export const EnvironmentalImpact = [
+  {
+    order_no: 1,
+    title: "Environmental impact",
+    questions: [
+      {
+        order_no: 1,
+        question:
+          "How has your organization assessed the overall environmental impact of this AI application? What factors were considered in this assessment?",
+        hint: "Summarize your environmental impact assessment, including the AI provider’s sustainability practices and your organization’s efforts to mitigate negative impacts.",
+        priority_level: "low priority",
+        answer_type: "Long text",
+        input_type: "Tiptap area",
+        evidence_required: false,
+        isrequired: true,
+        evidence_files: [],
+        dropdown_options: [],
+      },
+      {
+        order_no: 2,
+        question:
+          "What are the environmental effects of the AI application? How are these effects measured and mitigated?",
+        hint: "Describe the environmental effects of the AI system and how these are measured and mitigated through your and the provider’s practices.",
+        priority_level: "low priority",
+        answer_type: "Long text",
+        input_type: "Tiptap area",
+        evidence_required: false,
+        isrequired: true,
+        evidence_files: [],
+        dropdown_options: [],
+      },
+    ],
+  },
+];

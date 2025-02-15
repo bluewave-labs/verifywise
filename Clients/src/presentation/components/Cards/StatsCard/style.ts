0c45860e1688560e574b64d12a619106cf65c148
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-02-15 23:29:36 +0330

    StatsCard component added

---
+++ Clients/src/presentation/components/Cards/StatsCard/style.ts
@@ -0,0 +1,20 @@
+export const StatsCardFrame = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+  minWidth: "300px",
+  maxWidth: "100%",
+  gap: "40px",
+  backgroundColor: "white",
+  padding: "10px 25px",
+  border: "1px solid #EAECF0",
+  borderRadius: "4px",
+  boxShadow: "none",
+};
+
+export const StatsCardRate = {
+  color: "#2D3748",
+  fontSize: 40,
+};

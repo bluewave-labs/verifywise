4e4cda256705294fdd9b6514594b8057260015fb
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-02-18 18:12:56 +0330

    Fetching progress values

--- Clients/src/presentation/pages/Assessment/1.0AssessmentTracker/index.tsx
+++ Clients/src/presentation/pages/Assessment/1.0AssessmentTracker/index.tsx
@@ -2,27 +2,31 @@ import { useEffect, useState } from "react";
 import { Stack, Typography, useTheme } from "@mui/material";
 import { pageHeadingStyle } from "./index.style";
 import { getEntityById } from "../../../../application/repository/entity.repository";
+import StatsCard from "../../../components/Cards/StatsCard";
+import VWSkeleton from "../../../vw-v2-components/Skeletons";
 
 const AssessmentTracker = () => {
   const theme = useTheme();
-  const [userData, setUserData] = useState(null);
+  const [progressData, setProgressData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const fetchUserData = async () => {
+    const fetchProgressData = async () => {
       try {
-        const data = await getEntityById({
+        const response = await getEntityById({
           routeUrl: "/projects/assessment/progress/3",
         });
-        setUserData(data);
+        setProgressData(response.data);
       } catch (error) {
         console.error("Failed to fetch user data:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
-    fetchUserData();
+    fetchProgressData();
   }, []);
 
-  console.log("userData : ", userData);
   return (
     <Stack className="assessment-tracker">
       <Stack
@@ -33,6 +37,31 @@ const AssessmentTracker = () => {
         }}
       >
         <Typography sx={pageHeadingStyle}>Assessment tracker</Typography>
+        <Stack
+          sx={{ maxWidth: 1400, marginTop: "10px", gap: theme.spacing(10) }}
+        >
+          {loading ? (
+            <VWSkeleton
+              height={82}
+              minHeight={82}
+              minWidth={300}
+              width={"100%"}
+              key={1400}
+              variant="rectangular"
+            />
+          ) : progressData ? (
+            <StatsCard
+              total={progressData.totalQuestions}
+              completed={progressData.answeredQuestions}
+              title="Questions"
+              progressbarColor="#13715B"
+            />
+          ) : (
+            <Typography>
+              Unable to fetch statistical values from the server
+            </Typography>
+          )}
+        </Stack>
       </Stack>
     </Stack>
   );

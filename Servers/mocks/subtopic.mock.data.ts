8dfcf8b8c19361e1d1387c28041649ca2535e2e5
Harsh <pharsh4585@gmail.com>
2025-02-26 02:27:59 -0500

    added mock data for assessment tracker

--- Servers/mocks/subtopic.mock.data.ts
+++ Servers/mocks/subtopic.mock.data.ts
@@ -5,207 +5,193 @@ export const subtopics = (
 ): Subtopic[] => {
   return [
     {
-      id: 1,
       topic_id: topics[0],
+      title: "General",
+      order_no: 1
+    },
+    {
+      topic_id: topics[0],
+      title: "Technology details",
+      order_no: 2
+    },
+    {
+      topic_id: topics[1],
       title: "Transparency and provision of information to deployers",
       order_no: 1
     },
     {
-      id: 2,
-      topic_id: topics[0],
+      topic_id: topics[1],
       title: "Responsibilities along the AI value chain",
       order_no: 2
     },
     {
-      id: 3,
-      topic_id: topics[1],
+      topic_id: topics[2],
       title: "Responsibilities along the AI value chain",
       order_no: 1
     },
     {
-      id: 4,
-      topic_id: topics[1],
+      topic_id: topics[2],
       title: "Fundamental rights impact assessments for high-risk AI systems",
       order_no: 2
     },
     {
-      id: 5,
-      topic_id: topics[2],
-      title: "AI model capability assessment",
-      order_no: 1
-    },
-    {
-      id: 6,
       topic_id: topics[3],
       title: "AI model capability assessment",
       order_no: 1
     },
     {
-      id: 7,
       topic_id: topics[4],
+      title: "AI model capability assessment",
+      order_no: 1
+    },
+    {
+      topic_id: topics[5],
       title: "User notification of AI system use",
       order_no: 1
     },
     {
-      id: 8,
-      topic_id: topics[5],
+      topic_id: topics[6],
       title: "Oversight documentation",
       order_no: 1
     },
     {
-      id: 9,
-      topic_id: topics[5],
+      topic_id: topics[6],
       title: "Human intervention mechanisms",
       order_no: 2
     },
     {
-      id: 10,
-      topic_id: topics[6],
+      topic_id: topics[7],
       title: "System validation and reliability documentation",
       order_no: 1
     },
     {
-      id: 11,
-      topic_id: topics[6],
+      topic_id: topics[7],
       title: "AI system change documentation",
       order_no: 2
     },
     {
-      id: 12,
-      topic_id: topics[7],
+      topic_id: topics[8],
       title: "EU database registration",
       order_no: 1
     },
     {
-      id: 13,
-      topic_id: topics[8],
+      topic_id: topics[9],
       title: "Post-market monitoring by providers and post-market monitoring plan for high-risk AI systems",
       order_no: 1
     },
     {
-      id: 14,
-      topic_id: topics[9],
+      topic_id: topics[10],
       title: "Bias and fairness evaluation",
       order_no: 1
     },
     {
-      id: 15,
-      topic_id: topics[10],
+      topic_id: topics[11],
       title: "System information documentation",
       order_no: 1
     },
     {
-      id: 16,
-      topic_id: topics[11],
+      topic_id: topics[12],
       title: "Transparency obligations for providers and users of certain AI systems",
       order_no: 1
     },
     {
-      id: 17,
-      topic_id: topics[12],
+      topic_id: topics[13],
       title: "Environmental impact",
       order_no: 1
     },
 
     {
-      id: 18,
-      topic_id: topics[13],
+      topic_id: topics[14],
+      title: "General",
+      order_no: 1
+    },
+    {
+      topic_id: topics[14],
+      title: "Technology details",
+      order_no: 2
+    },
+    {
+      topic_id: topics[15],
       title: "Transparency and provision of information to deployers",
       order_no: 1
     },
     {
-      id: 19,
-      topic_id: topics[13],
+      topic_id: topics[15],
       title: "Responsibilities along the AI value chain",
       order_no: 2
     },
     {
-      id: 20,
-      topic_id: topics[14],
+      topic_id: topics[16],
       title: "Responsibilities along the AI value chain",
       order_no: 1
     },
     {
-      id: 21,
-      topic_id: topics[14],
+      topic_id: topics[16],
       title: "Fundamental rights impact assessments for high-risk AI systems",
       order_no: 2
     },
     {
-      id: 22,
-      topic_id: topics[15],
-      title: "AI model capability assessment",
-      order_no: 1
-    },
-    {
-      id: 23,
-      topic_id: topics[16],
-      title: "AI model capability assessment",
-      order_no: 1
-    },
-    {
-      id: 24,
       topic_id: topics[17],
+      title: "AI model capability assessment",
+      order_no: 1
+    },
+    {
+      topic_id: topics[18],
+      title: "AI model capability assessment",
+      order_no: 1
+    },
+    {
+      topic_id: topics[19],
       title: "User notification of AI system use",
       order_no: 1
     },
     {
-      id: 25,
-      topic_id: topics[18],
+      topic_id: topics[20],
       title: "Oversight documentation",
       order_no: 1
     },
     {
-      id: 26,
-      topic_id: topics[18],
+      topic_id: topics[20],
       title: "Human intervention mechanisms",
       order_no: 2
     },
     {
-      id: 27,
-      topic_id: topics[19],
+      topic_id: topics[21],
       title: "System validation and reliability documentation",
       order_no: 1
     },
     {
-      id: 28,
-      topic_id: topics[19],
+      topic_id: topics[21],
       title: "AI system change documentation",
       order_no: 2
     },
     {
-      id: 29,
-      topic_id: topics[20],
+      topic_id: topics[22],
       title: "EU database registration",
       order_no: 1
     },
     {
-      id: 30,
-      topic_id: topics[21],
+      topic_id: topics[23],
       title: "Post-market monitoring by providers and post-market monitoring plan for high-risk AI systems",
       order_no: 1
     },
     {
-      id: 31,
-      topic_id: topics[22],
+      topic_id: topics[24],
       title: "Bias and fairness evaluation",
       order_no: 1
     },
     {
-      id: 32,
-      topic_id: topics[23],
+      topic_id: topics[25],
       title: "System information documentation",
       order_no: 1
     },
     {
-      id: 33,
-      topic_id: topics[24],
+      topic_id: topics[26],
       title: "Transparency obligations for providers and users of certain AI systems",
       order_no: 1
     },
     {
-      id: 34,
-      topic_id: topics[25],
+      topic_id: topics[27],
       title: "Environmental impact",
       order_no: 1
     },
--- Servers/mocks/topic.mock.data.ts
+++ Servers/mocks/topic.mock.data.ts
@@ -3,157 +3,141 @@ import { Topic } from "../models/topic.model";
 export const topics = (assessment1: number, assessment2: number): Topic[] => {
   return [
     {
-      id: 1,
+      assessment_id: assessment1,
+      title: "Project scope",
+      order_no: 0
+    },
+    {
       assessment_id: assessment1,
       title: "Risk management system",
       order_no: 1
     },
     {
-      id: 2,
       assessment_id: assessment1,
       title: "Data governance",
       order_no: 2
     },
     {
-      id: 3,
       assessment_id: assessment1,
       title: "Technical documentation",
       order_no: 3
     },
     {
-      id: 4,
       assessment_id: assessment1,
       title: "Record keeping",
       order_no: 4
     },
     {
-      id: 5,
       assessment_id: assessment1,
       title: "Transparency & user information",
       order_no: 5
     },
     {
-      id: 6,
       assessment_id: assessment1,
       title: "Human oversight",
       order_no: 6
     },
     {
-      id: 7,
       assessment_id: assessment1,
       title: "Accuracy, robustness, cyber security",
       order_no: 7
     },
     {
-      id: 8,
       assessment_id: assessment1,
       title: "Conformity assessment",
       order_no: 8
     },
     {
-      id: 9,
       assessment_id: assessment1,
       title: "Post-market monitoring",
       order_no: 9
     },
     {
-      id: 10,
       assessment_id: assessment1,
       title: "Bias monitoring and mitigation",
       order_no: 10
     },
     {
-      id: 11,
       assessment_id: assessment1,
       title: "Accountability and governance",
       order_no: 11
     },
     {
-      id: 12,
       assessment_id: assessment1,
       title: "Explainability",
       order_no: 12
     },
     {
-      id: 13,
       assessment_id: assessment1,
       title: "Environmental impact",
       order_no: 13
     },
     {
-      id: 14,
+      assessment_id: assessment2,
+      title: "Project scope",
+      order_no: 0
+    },
+    {
       assessment_id: assessment2,
       title: "Risk management system",
       order_no: 1
     },
     {
-      id: 15,
       assessment_id: assessment2,
       title: "Data governance",
       order_no: 2
     },
     {
-      id: 16,
       assessment_id: assessment2,
       title: "Technical documentation",
       order_no: 3
     },
     {
-      id: 17,
       assessment_id: assessment2,
       title: "Record keeping",
       order_no: 4
     },
     {
-      id: 18,
       assessment_id: assessment2,
       title: "Transparency & user information",
       order_no: 5
     },
     {
-      id: 19,
       assessment_id: assessment2,
       title: "Human oversight",
       order_no: 6
     },
     {
-      id: 20,
       assessment_id: assessment2,
       title: "Accuracy, robustness, cyber security",
       order_no: 7
     },
     {
-      id: 21,
       assessment_id: assessment2,
       title: "Conformity assessment",
       order_no: 8
     },
     {
-      id: 22,
       assessment_id: assessment2,
       title: "Post-market monitoring",
       order_no: 9
     },
     {
-      id: 23,
       assessment_id: assessment2,
       title: "Bias monitoring and mitigation",
       order_no: 10
     },
     {
-      id: 24,
       assessment_id: assessment2,
       title: "Accountability and governance",
       order_no: 11
     },
     {
-      id: 25,
       assessment_id: assessment2,
       title: "Explainability",
       order_no: 12
     },
     {
-      id: 26,
       assessment_id: assessment2,
       title: "Environmental impact",
       order_no: 13

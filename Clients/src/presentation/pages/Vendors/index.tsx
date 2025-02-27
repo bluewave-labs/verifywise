154a2034c688d0d57d2a86210ba4f0751409c624
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2025-02-27 13:45:43 -0500

    add new risk modal to risks tab

--- Clients/src/presentation/pages/Vendors/index.tsx
+++ Clients/src/presentation/pages/Vendors/index.tsx
@@ -22,10 +22,12 @@ import CustomStep from "../../components/PageTour/CustomStep";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
+import AddNewRisk from "../../components/Modals/NewRisk";
 
 const Vendors = () => {
   const theme = useTheme();
   const [isOpen, setIsOpen] = useState(false);
+  const [isRiskModalOpen, setIsRiskModalOpen] = useState(false);
   const [value, setValue] = useState("1");
   const [vendorChangeTrigger, setVendorChangeTrigger] = useState(0);
   const [vendorRiskChangeTrigger, setVendorRiskChangeTrigger] = useState(0);
@@ -334,6 +336,13 @@ const Vendors = () => {
         setIsOpen={() => setIsOpen(false)}
         value={value}
         onVendorChange={updateVendorChangeTrigger}
+      />
+      <AddNewRisk
+        isOpen={isRiskModalOpen}
+        handleChange={handleChange}
+        setIsOpen={() => setIsRiskModalOpen(false)}
+        value={value}
+        onRiskChange={updateVendorRiskChangeTrigger}
       />
     </div>
   );

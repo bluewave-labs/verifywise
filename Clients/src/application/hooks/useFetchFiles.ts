d7323b3c6a361c9f20480f52da29e3df10fc627c
salmanja <71104867+salmanja@users.noreply.github.com>
2025-03-21 18:26:11 -0400

    optimized fetch files hook: imported file interface,changed hook and variable to FilesData, ensured uploadDate has proper fallback validation, accepted projectID as parameter to fetch files for specific project

--- Clients/src/application/hooks/useFetchFiles.ts
+++ Clients/src/application/hooks/useFetchFiles.ts
@@ -1,48 +1,40 @@
 import { useState, useEffect } from "react";
 import { getEntityById } from "../repository/entity.repository";
+import { File} from "../../domain/File"
 
-interface FileData {
-  id: string;
-  name: string;
-  type: string;
-  uploadDate: string;
-  uploader: string;
-}
-
-export const useFetchFiles = () => {
-  const [files, setFiles] = useState<FileData[]>([]);
+export const useFetchFiles = (projectID:string) => {
+  const [filesData, setFilesData] = useState<File[]>([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchAllFiles = async () => {
       try {
         setLoading(true);
-        const filesData: FileData[] = await getEntityById({
-          routeUrl: "/files",
-        });
+       const routeUrl = projectID ? `/files?projectID=${projectID}` : "/files";
+       const filesResponse = await getEntityById({ routeUrl });
 
-        if (filesData && Array.isArray(filesData)) {
-          const formattedFiles: FileData[] = filesData.map((file) => ({
-            id: file.id,
-            name: file.name,
-            type: file.type || "N/A",
-            uploadDate: new Date(file.uploadDate).toLocaleDateString(),
-            uploader: file.uploader || "N/A",
-          }));
-          setFiles(formattedFiles);
+        if (filesResponse && Array.isArray(filesResponse)) {
+          setFilesData(
+            filesResponse.map((file) => ({
+              id: file.id,
+              name: file.name,
+              type: file.type || "N/A",
+              uploadDate: file.uploadDate ? new Date(file.uploadDate).toLocaleDateString() : "Invalid Date",
+              uploader: file.uploader || "N/A",
+            }))
+          );
         } else {
-          setFiles([]);
+            setFilesData([]);
         }
-      } catch (error) {
-        console.error("Error fetching files", error);
-        setFiles([]);
-      } finally {
+    } catch (error) {
+        console.error("error fetching files",error);
+        setFilesData([]);
+    } finally {
         setLoading(false);
-      }
+    }
     };
-
     fetchAllFiles();
-  }, []);
+  }, [projectID]);
 
-  return { files, loading };
+  return { filesData, loading };
 };

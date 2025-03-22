3c919c1a9ce2ba34536716b37dad689d0aac981a
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2025-03-22 11:37:26 -0400

    capitalization

--- Clients/src/presentation/components/Modals/NewRisk/index.tsx
+++ Clients/src/presentation/components/Modals/NewRisk/index.tsx
@@ -615,7 +615,7 @@ const AddNewRisk: React.FC<AddNewRiskProps> = ({
               fontWeight={600}
               marginBottom={theme.spacing(5)}
             >
-              {existingRisk ? "Edit Risk" : "Add new Risk"}
+              {existingRisk ? "Edit risk" : "Add new risk"}
             </Typography>
             <Close style={{ cursor: "pointer" }} onClick={setIsOpen} />
           </Stack>

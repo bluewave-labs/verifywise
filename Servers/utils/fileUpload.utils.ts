bdbb5989c2397ca2d49c5fd06afdc6bd6df3d891
Harsh <pharsh4585@gmail.com>
2025-04-03 17:06:45 -0400

    updated autodriver

--- Servers/utils/fileUpload.utils.ts
+++ Servers/utils/fileUpload.utils.ts
@@ -2,18 +2,24 @@ import { UploadedFile } from "./question.utils";
 import { sequelize } from "../database/db";
 import { FileModel } from "../models/file.model";
 import { QueryTypes } from "sequelize";
+import { ProjectModel } from "../models/project.model";
 
 export const uploadFile = async (
   file: UploadedFile,
   user_id: number,
   project_id: number
 ) => {
+  const projectIsDemo = await sequelize.query(
+    "SELECT is_demo FROM projects WHERE id = :id",
+    { replacements: { id: project_id }, mapToModel: true, model: ProjectModel },
+  )
+  const is_demo = projectIsDemo[0].is_demo || false
   const query = `INSERT INTO files
     (
-      filename, content, project_id, uploaded_by, uploaded_time
+      filename, content, project_id, uploaded_by, uploaded_time, is_demo
     )
     VALUES (
-      :filename, :content, :project_id, :uploaded_by, :uploaded_time
+      :filename, :content, :project_id, :uploaded_by, :uploaded_time, :is_demo
     ) RETURNING *`;
   const result = await sequelize.query(query, {
     replacements: {
@@ -21,7 +27,8 @@ export const uploadFile = async (
       content: file.buffer,
       project_id,
       uploaded_by: user_id,
-      uploaded_time: new Date().toISOString()
+      uploaded_time: new Date().toISOString(),
+      is_demo
     },
     mapToModel: true,
     model: FileModel,

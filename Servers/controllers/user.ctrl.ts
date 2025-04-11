d83e287ddfde1d3ec5ac2e9876e76fe369905ac7
Samuel Coutinho <sfcrp2000@gmail.com>
2025-04-11 11:28:46 -0400

    update refresh token cookie path for improved routing

--- Servers/controllers/user.ctrl.ts
+++ Servers/controllers/user.ctrl.ts
@@ -125,7 +125,7 @@ async function loginUser(req: Request, res: Response): Promise<any> {
         );
         res.cookie("refresh_token", refreshToken, {
           httpOnly: true,
-          path: "/users/refresh-token",
+          path: "/users",
           expires: new Date(Date.now() + 1 * 3600 * 1000 * 24 * 30), // 30 days
         });
         return res.status(202).json(

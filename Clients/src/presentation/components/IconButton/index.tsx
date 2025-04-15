a0f5bf7f08c78a8a9aee25112e49b65fd73687ca
salmanja <71104867+salmanja@users.noreply.github.com>
2025-04-15 16:04:58 -0400

    added dynamic listOfButtons array, based on types

--- Clients/src/presentation/components/IconButton/index.tsx
+++ Clients/src/presentation/components/IconButton/index.tsx
@@ -113,6 +113,14 @@ const IconButton: React.FC<IconButtonProps> = ({
     }
   }
 
+  /** 
+   * List of context-specific dropdown actions used to render menu items.
+   * 
+   * - For type "report", the menu item will be "download".
+   * - For other types (e.g. "Vendor"), the menu item will be "edit", "remove".
+   */
+const listOfButtons = type === "report" ? ["download"] : ["edit", "remove"];
+
   /**
    * A dropdown list of options rendered as a Material-UI Menu component.
    *

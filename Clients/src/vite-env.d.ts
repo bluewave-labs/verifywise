72510e2add0a0e6578ebf2cbb4a5d95055fdb3de
Samuel Coutinho <sfcrp2000@gmail.com>
2025-04-22 10:15:49 -0400

    Add DemoAppBanner component and update environment variable types

--- Clients/src/vite-env.d.ts
+++ Clients/src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_IS_DEMO_APP: string; 
+}

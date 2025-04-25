9ce5eff1c3126f9b605f591352797069c3d7bcca
eem <eimon1150@gmail.com>
2025-04-25 08:57:31 -0600

    Add authenticateJWT token in generate-report GET request

--- Servers/routes/reporting.route.ts
+++ Servers/routes/reporting.route.ts
@@ -2,7 +2,9 @@ import express from "express";
 const router = express.Router();
 import { getProjectRiskReports } from "../controllers/reporting.ctrl";
 
+import authenticateJWT from "../middleware/auth.middleware";
+
 // GET request
-router.get("/generate-report/:id", getProjectRiskReports)
+router.get("/generate-report/:id", authenticateJWT, getProjectRiskReports)
 
 export default router;

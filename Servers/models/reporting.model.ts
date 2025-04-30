d5e641a234715c21a3a438183481b8697aa22314
eem <eimon1150@gmail.com>
2025-04-30 02:50:54 -0600

    Minor bug fix for report type condition

--- Servers/models/reporting.model.ts
+++ Servers/models/reporting.model.ts
@@ -1,5 +1,5 @@
 export const DefaultReportName = Object.freeze({
-  RISK_REPORT: 'risk-report',
+  PROJECTRISK_REPORT: 'risk-report',
   VENDOR_REPORT: 'vendor-report',
   ASSESSMENT_REPORT: 'assessment-tracker-report',
   COMPLIANCE_REPORT: 'compliance-tracker-report',

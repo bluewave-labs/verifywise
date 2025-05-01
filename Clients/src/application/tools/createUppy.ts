4aee7e799e0ea14a1154062c5aeeaf1e39dcd52c
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-05-01 20:18:12 +0330

    matching type directories

--- Clients/src/application/tools/createUppy.ts
+++ Clients/src/application/tools/createUppy.ts
@@ -1,7 +1,7 @@
 import Uppy from "@uppy/core";
 import XHRUpload from "@uppy/xhr-upload";
 import { ENV_VARs } from "../../../env.vars";
-import { FileData } from "../../domain/File";
+import { FileData } from "../../domain/types/File";
 
 interface UppyProps {
   meta?: Record<string, unknown>;

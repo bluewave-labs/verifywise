cdd247e3ac4f41b38e09f8b780215bea5a5cb24f
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-05-05 12:38:27 +0330

    Inputs/Autocomplete, separating interfaces

--- Clients/src/domain/interfaces/iWidget.ts
+++ Clients/src/domain/interfaces/iWidget.ts
@@ -1,3 +1,7 @@
+import { Theme } from "@mui/material";
+
+import { SxProps } from "@mui/material";
+
 /**
  * Props for the CloseButton component.
  *
@@ -16,4 +20,22 @@ export interface IconButtonProps {
   warningMessage: string;
   type: string;
   onMouseEvent: (event: React.SyntheticEvent) => void;
+}
+
+export interface AutoCompleteOption {
+  _id: string;
+  name: string;
+}
+
+export interface AutoCompleteFieldProps {
+  id: string;
+  type: string;
+  options?: AutoCompleteOption[];
+  placeholder?: string;
+  disabled?: boolean;
+  sx?: SxProps<Theme>;
+  width?: number | string;
+  autoCompleteValue: AutoCompleteOption | undefined;
+  setAutoCompleteValue: (value: AutoCompleteOption | undefined) => void;
+  error?: string;
 }

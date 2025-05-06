8e4f5c57212b1a47c6335d86205759bf48a8c312
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-05-06 10:04:00 +0330

    Inputs/Modals/Basic, separating interfaces and hard-coded value

---
+++ Clients/src/presentation/components/Modals/Basic/style.ts
@@ -0,0 +1,22 @@
+export const BasicModalCancelButtonStyle = {
+  width: 100,
+  textTransform: "capitalize",
+  fontSize: 13,
+  borderRadius: "4px",
+  "&:hover": {
+    boxShadow: "none",
+    backgroundColor: "transparent",
+  },
+};
+
+export const BasicModalDeleteButtonStyle = {
+  width: 140,
+  fontSize: 13,
+  backgroundColor: "#DB504A",
+  border: "1px solid #DB504A",
+  boxShadow: "none",
+  borderRadius: "4px",
+  "&:hover": {
+    boxShadow: "none",
+  },
+};

947f5875ed46027e34430654af34b5905c264944
salmanja <71104867+salmanja@users.noreply.github.com>
2025-05-06 15:55:17 -0400

    removed dashboard values and cleaned up code

--- Clients/src/presentation/pages/FileManager/index.tsx
+++ Clients/src/presentation/pages/FileManager/index.tsx
@@ -1,7 +1,6 @@
 import React, {
   useState,
   useEffect,
-  useContext,
   useMemo,
   forwardRef,
 } from "react";
@@ -14,7 +13,6 @@ import { vwhomeHeading } from "../Home/1.0Home/style";
 import { useFetchFiles } from "../../../application/hooks/useFetchFiles";
 import { useProjectData } from "../../../application/hooks/useFetchProjects";
 import FileTable from "../../components/Table/FileTable/FileTable";
-import { VerifyWiseContext } from "../../../application/contexts/VerifyWise.context";
 import { filesTableFrame, filesTablePlaceholder } from "./styles";
 import ProjectFilterDropdown from "../../components/Inputs/Dropdowns/ProjectFilter/ProjectFilterDropdown";
 
@@ -42,7 +40,6 @@ const COLUMNS: Column[] = COLUMN_NAMES.map((name, index) => ({
  * @returns {JSX.Element} The FileManager component.
  */
 const FileManager: React.FC = (): JSX.Element => {
-  const { dashboardValues } = useContext(VerifyWiseContext);
   const theme = useTheme();
   const [runFileTour, setRunFileTour] = useState(false);
   const { refs, allVisible } = useMultipleOnScreen<HTMLDivElement>({

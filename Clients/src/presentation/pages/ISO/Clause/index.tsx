01c8710497dcda7df7cefbaf9f2ce148c3d82870
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-05-07 11:34:15 +0330

    listing hard-coded clause categories

--- Clients/src/presentation/pages/ISO/Clause/index.tsx
+++ Clients/src/presentation/pages/ISO/Clause/index.tsx
@@ -1,4 +1,13 @@
-import { Stack, Typography } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Stack,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { accordionStyle } from "../style";
+import { useState } from "react";
 
 const ISO42001ClauseList = [
   {
@@ -38,15 +47,68 @@ const ISO42001ClauseList = [
 ];
 
 const ISO42001Clauses = () => {
+  const [expanded, setExpanded] = useState<number | false>(false);
+
+  const handleAccordionChange =
+    (panel: number) => (_: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+
   return (
-    <Stack className="iso-42001-clauses">
+    <Stack
+      className="iso-42001-clauses"
+      sx={{
+        maxWidth: "1400px",
+        marginTop: "14px",
+        gap: "20px",
+      }}
+    >
       {ISO42001ClauseList.map((clause) => (
-        <Typography
-          key={clause.id}
-          sx={{ color: "#1A1919", fontWeight: 600, mb: "6px", fontSize: 16 }}
-        >
-          {clause.title} {" Clauses"}
-        </Typography>
+        <>
+          <Typography
+            key={clause.id}
+            sx={{ color: "#1A1919", fontWeight: 600, mb: "6px", fontSize: 16 }}
+          >
+            {clause.title} {" Clauses"}
+          </Typography>
+          {clause.clauses.map((clause) => (
+            <Stack key={clause.number}>
+              <Accordion
+                key={clause.number}
+                expanded={true}
+                sx={accordionStyle}
+                onChange={handleAccordionChange(clause.number ?? 0)}
+              >
+                <AccordionSummary
+                  sx={{
+                    backgroundColor: "#fafafa",
+                    flexDirection: "row-reverse",
+                  }}
+                  expandIcon={
+                    <ExpandMoreIcon
+                      sx={{
+                        transform:
+                          expanded === clause.number
+                            ? "rotate(180deg)"
+                            : "rotate(270deg)",
+                        transition: "transform 0.5s ease-in",
+                      }}
+                    />
+                  }
+                >
+                  <Typography
+                    sx={{
+                      paddingLeft: "2.5px",
+                    }}
+                  >
+                    {"Clause "} {clause.number} {" : "} {clause.title}
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails sx={{ padding: 0 }}></AccordionDetails>
+              </Accordion>
+            </Stack>
+          ))}
+        </>
       ))}
     </Stack>
   );

600db990f77d79af17c97ea0f967d74ac8736690
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-05-08 12:24:51 +0330

    annex hard-coded structure

--- Clients/src/presentation/pages/ISO/Annex/index.tsx
+++ Clients/src/presentation/pages/ISO/Annex/index.tsx
@@ -1,5 +1,7 @@
+import { Stack } from "@mui/material";
+
 const ISO42001Annex = () => {
-  return <div>ISO42001Annex</div>;
+  return <Stack className="iso-42001-annex">ISO42001Annex</Stack>;
 };
 
 export default ISO42001Annex;

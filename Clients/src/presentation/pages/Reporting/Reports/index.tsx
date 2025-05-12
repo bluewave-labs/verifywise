0c5e557fdd2453fd9bf15eb658ed4534fb1c7de2
eem <eimon1150@gmail.com>
2025-05-12 00:55:20 -0600

    Add success and error toasts for deleteing report

--- Clients/src/presentation/pages/Reporting/Reports/index.tsx
+++ Clients/src/presentation/pages/Reporting/Reports/index.tsx
@@ -1,38 +1,55 @@
 import { useState, useContext, lazy, Suspense } from 'react'
-import { Stack } from '@mui/material';
+import { Stack, Box } from '@mui/material';
 const ReportTable = lazy(() => import('../../../components/Table/ReportTable'));
 import { VerifyWiseContext } from '../../../../application/contexts/VerifyWise.context';
 import { TITLE_OF_COLUMNS } from './constants';
 import useGeneratedReports from '../../../../application/hooks/useGeneratedReports';
 import {styles} from './styles';
 import { deleteEntityById } from '../../../../application/repository/entity.repository';
+import { handleAlert } from '../../../../application/tools/alertUtils';
+import Alert from '../../../components/Alert';
 
 const Reports = () => {
   const { dashboardValues } = useContext(VerifyWiseContext);
   const { selectedProjectId} = dashboardValues;
   const projectId = selectedProjectId;
   const [currentPage, setCurrentPage] = useState(0);
+  const [alert, setAlert] = useState<{
+    variant: "success" | "info" | "warning" | "error";
+    title?: string;
+    body: string;
+  } | null>(null);
 
   const {
     generatedReports
   } = useGeneratedReports({ projectId });
 
+  const handleToast = (type: any, message: string) => {
+    handleAlert({
+      variant: type,
+      body: message,
+      setAlert,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  };
+
   const handleRemoveReport = async (id: number) => {
-    // function for remove report 
-    console.log('id:', id)
     try {
       const response = await deleteEntityById({
         routeUrl: `/reporting/${id}`,
       });
       if (response.status === 200) {
-        console.log("success", "Report deleted successfully.");
-      } else if (response.status === 404) {
-        console.log("error", "Report not found.");
+        handleToast("success", "Report deleted successfully.");
+      } else if (response.status === 204) {
+        handleToast("error", "Report not found.");
       } else {
-        console.log("error", "Unexpected error occurs. Report delete fails.");
+        handleToast("error", "Unexpected error occurs. Report delete fails.");
       }
     } catch (error) {
       console.error("Error sending request", error);
+      handleToast("error", "Report delete fails.");
     }
   }
 
@@ -42,6 +59,19 @@ const Reports = () => {
 
   return (
     <Stack sx={styles.tableContainer}>
+      {alert && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Box>
+            <Alert
+              variant={alert.variant}
+              title={alert.title}
+              body={alert.body}
+              isToast={true}
+              onClick={() => setAlert(null)}
+            />
+          </Box>
+        </Suspense>
+      )}
       <Suspense fallback={<div>Loading...</div>}>
         <ReportTable
           columns={TITLE_OF_COLUMNS}

716788a223bb741aa4e2a295b5a5824cb3793083
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-05-14 12:33:48 +0330

    Organization api test.rest file added

--- Servers/routes/organization.route.ts
+++ Servers/routes/organization.route.ts
@@ -24,32 +24,32 @@ import {
   getOrganizationProjects,
 } from "../controllers/organization.ctrl";
 
-import authenticateJWT from "../middleware/auth.middleware";
+// import authenticateJWT from "../middleware/auth.middleware";
 
 // GET requests
-router.get("/", authenticateJWT, getAllOrganizations);
-router.get("/:id", authenticateJWT, getOrganizationById);
-router.get("/:id/members", authenticateJWT, getOrganizationMembers);
-router.get("/:id/projects", authenticateJWT, getOrganizationProjects);
+router.get("/", /*authenticateJWT,*/ getAllOrganizations);
+router.get("/:id", /*authenticateJWT,*/ getOrganizationById);
+router.get("/:id/members", /*authenticateJWT,*/ getOrganizationMembers);
+router.get("/:id/projects", /*authenticateJWT,*/ getOrganizationProjects);
 
 // POST requests
-router.post("/", authenticateJWT, createOrganization);
-router.post("/:id/members", authenticateJWT, addMemberToOrganization);
-router.post("/:id/projects", authenticateJWT, addProjectToOrganization);
+router.post("/", /*authenticateJWT,*/ createOrganization);
+router.post("/:id/members", /*authenticateJWT,*/ addMemberToOrganization);
+router.post("/:id/projects", /*authenticateJWT,*/ addProjectToOrganization);
 
 // PATCH requests
-router.patch("/:id", authenticateJWT, updateOrganizationById);
+router.patch("/:id", /*authenticateJWT,*/ updateOrganizationById);
 
 // DELETE requests
-router.delete("/:id", authenticateJWT, deleteOrganizationById);
+router.delete("/:id", /*authenticateJWT,*/ deleteOrganizationById);
 router.delete(
   "/:id/members/:memberId",
-  authenticateJWT,
+  /*authenticateJWT,*/
   removeMemberFromOrganization
 );
 router.delete(
   "/:id/projects/:projectId",
-  authenticateJWT,
+  /*authenticateJWT,*/
   removeProjectFromOrganization
 );
 

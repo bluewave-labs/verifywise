f9b91f465d2a7c1b467d21220a99af489f03d33c
Aryaman Kumar Sharma <kumararyaman26@gmail.com>
2025-05-17 00:10:51 +0530

    Fix: TabList Indicator Width

--- Clients/src/presentation/pages/Vendors/style.ts
+++ Clients/src/presentation/pages/Vendors/style.ts
@@ -1,10 +1,11 @@
 export const tabStyle = {
     textTransform: "none",
     fontWeight: 400,
-    alignItems: "flex-start",
+    alignItems: "center",
     justifyContent: "flex-end",
     padding: "16px 0 7px",
     minHeight: "20px",
+    minWidth: "auto",
     "&.Mui-selected": {
       color: "#13715B",
     },

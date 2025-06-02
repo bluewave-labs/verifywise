8287520674aa9e40903ee5643bae4a0c29f38772
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-06-02 09:53:24 +0330

    CreateMyOrganization added

--- Clients/src/application/repository/organization.repository.ts
+++ Clients/src/application/repository/organization.repository.ts
@@ -1,6 +1,7 @@
 import { GetRequestParams } from "../../domain/interfaces/iRequestParams";
 import { apiServices } from "../../infrastructure/api/networkServices";
 import { getAuthToken } from "../redux/getAuthToken";
+import { RequestParams } from "../../domain/interfaces/iRequestParams";
 
 /**
  * Retrieves the current user's organization details.
@@ -25,6 +26,30 @@ export async function GetMyOrganization({
     return response.data;
   } catch (error) {
     console.error("Error getting organization details:", error);
+    throw error;
+  }
+}
+
+/**
+ * Creates a new organization for the current user.
+ *
+ * @param {RequestParams} params - The parameters for creating a new organization.
+ * @returns {Promise<any>} A promise that resolves to the response data of the created organization.
+ * @throws Will throw an error if the organization creation fails.
+ */
+export async function CreateMyOrganization({
+  routeUrl = "/organizations",
+  body,
+  authToken = getAuthToken(),
+}: RequestParams): Promise<any> {
+  try {
+    const response = await apiServices.post(routeUrl, body, {
+      headers: { Authorization: `Bearer ${authToken}` },
+    });
+    console.log("Organization created successfully:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating organization:", error);
     throw error;
   }
 }

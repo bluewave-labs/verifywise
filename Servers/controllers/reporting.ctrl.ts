a9f15e2771bd93443500fc790f8dbee82bcd2c37
Samuel Coutinho <sfcrp2000@gmail.com>
2025-06-03 15:32:32 -0400

    Refactor report generation logic to streamline file upload process and improve error handling

--- Servers/controllers/reporting.ctrl.ts
+++ Servers/controllers/reporting.ctrl.ts
@@ -2,8 +2,7 @@ import { Request, Response } from "express";
 import { STATUS_CODE } from "../utils/statusCode.utils";
 import { uploadFile } from "../utils/fileUpload.utils";
 import {
-  getReportData,
-  isAuthorizedUser,
+  getReportData,  
   getFormattedReportName,
 } from "../services/reportService";
 import {
@@ -11,7 +10,7 @@ import {
   getGeneratedReportsQuery,
   getReportByIdQuery,
 } from "../utils/reporting.utils";
-import marked from "marked";
+import { marked } from "marked";
 import { sequelize } from "../database/db";
 const htmlDocx = require("html-to-docx");
 
@@ -54,7 +53,7 @@ export async function generateReports(
   req: Request,
   res: Response
 ): Promise<any> {
-  // const transaction = await sequelize.transaction();
+
   try {
     const {
       projectId: projectIdRaw,
@@ -73,65 +72,61 @@ export async function generateReports(
     if (typeof userId !== "number" || isNaN(userId)) {
       return res.status(400).json(STATUS_CODE[400]("Invalid user ID"));
     }
-    const authorizedUser = await isAuthorizedUser(projectId, userId); // check whether the user is authorized to download the report or not
+
     const reportData = {
       projectTitle, projectOwner
     };
-    if (authorizedUser) {
-      const markdownData = getReportData(
+
+    const markdownData = getReportData(
+      projectId,
+      frameworkId,
+      reportType,
+      reportData
+    );
+    const markdownDoc = await marked.parse(await markdownData); // markdown file
+    const generatedDoc = await htmlDocx(markdownDoc); // convert markdown to docx
+
+    let defaultFileName = getFormattedReportName(reportName, reportType);
+    const docFile = {
+      originalname: `${defaultFileName}.docx`,
+      buffer: generatedDoc,
+      fieldname: "file",
+      mimetype:
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    };
+
+    let uploadedFile;
+    try {
+      uploadedFile = await uploadFile(
+        docFile,
+        userId,
         projectId,
-        frameworkId,
-        reportType,
-        reportData
+        mapReportTypeToFileSource(reportType)
       );
-      const markdownDoc = await marked.parse(await markdownData); // markdown file
-      const generatedDoc = await htmlDocx(markdownDoc); // convert markdown to docx
+    } catch (error) {
+      console.error("File upload error:", error);
+      return res
+        .status(500)
+        .json(STATUS_CODE[500]("Error uploading report file"));
+    }
 
-      let defaultFileName = getFormattedReportName(reportName, reportType);
-      const docFile = {
-        originalname: `${defaultFileName}.docx`,
-        buffer: generatedDoc,
-        fieldname: "file",
-        mimetype:
-          "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-      };
-
-      let uploadedFile;
-      try {
-        uploadedFile = await uploadFile(
-          docFile,
-          userId,
-          projectId,
-          mapReportTypeToFileSource(reportType)
-        );
-      } catch (error) {
-        console.error("File upload error:", error);
-        return res
-          .status(500)
-          .json(STATUS_CODE[500]("Error uploading report file"));
-      }
-
-      if (uploadedFile) {
-        res.setHeader(
-          "Content-Disposition",
-          `attachment; filename="${uploadedFile.filename}"`
-        );
-        res.setHeader(
-          "Content-Type",
-          "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-        );
-        res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
-        return res.status(200).send(uploadedFile.content);
-      } else {
-        return res
-          .status(500)
-          .json(STATUS_CODE[500]("Error uploading report file"));
-      }
+    if (uploadedFile) {
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename="${uploadedFile.filename}"`
+      );
+      res.setHeader(
+        "Content-Type",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      );
+      res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+      return res.status(200).send(uploadedFile.content);
     } else {
       return res
-        .status(403)
-        .json(STATUS_CODE[403]("Unauthorized user to download the report."));
+        .status(500)
+        .json(STATUS_CODE[500]("Error uploading report file"));
     }
+
   } catch (error) {
     return res.status(500).json(STATUS_CODE[500]((error as Error).message));
   }

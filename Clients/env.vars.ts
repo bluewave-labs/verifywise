e9d53f84a3e2307e15f6a4b0734af6c4b2e6171d
Samuel Coutinho <sfcrp2000@gmail.com>
2025-06-12 12:30:52 -0400

    Fix: Update API base URL to also use environment variable

--- Clients/env.vars.ts
+++ Clients/env.vars.ts
@@ -1,4 +1,4 @@
 export const ENV_VARs = {
-  URL: `${window.location.protocol}//${window.location.hostname}:3000` || 'http://localhost:3000/',
+  URL: import.meta.env.VITE_APP_API_BASE_URL || `${window.location.protocol}//${window.location.hostname}:3000` || 'http://localhost:3000/',
   IS_DEMO_APP: import.meta.env.VITE_IS_DEMO_APP === 'true',
 };

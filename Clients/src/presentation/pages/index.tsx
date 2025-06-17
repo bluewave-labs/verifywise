e2da072723d323b5538132a28d121c8a9a2ad5eb
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-06-17 13:07:14 +0330

    Clearing playground

--- Clients/src/presentation/pages/index.tsx
+++ Clients/src/presentation/pages/index.tsx
@@ -14,11 +14,6 @@ const Playground = () => {
         padding: "20px",
       }}
     >
-      <Guider
-        link="https://www.iso.org/standard/81230.html"
-        title="ISO 42001"
-        description="ISO/IEC 42001 aims to ensure that AI systems are developed and used responsibly by promoting trust, transparency, and accountability."
-      />
     </Stack>
   );
 };

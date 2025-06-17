5ba7862ee502eaedc50e0fd625112c9082a0c9c1
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-06-17 12:37:05 +0330

    Helper/Guide component tooltip completed

--- Clients/src/presentation/components/Helpers/Guider/index.tsx
+++ Clients/src/presentation/components/Helpers/Guider/index.tsx
@@ -1,14 +1,40 @@
-import { Stack } from "@mui/material";
+import { Link, Stack, Tooltip, Typography } from "@mui/material";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import { GuiderStyler } from "./style";
 
-const Guider = () => {
+const Guider = ({
+  title,
+  description,
+  link,
+}: {
+  title: string;
+  description: string;
+  link: string;
+}) => {
   const theme = GuiderStyler();
 
   return (
-    <Stack component={"div"} sx={theme.helperFrameStyle}>
-      <QuestionMarkIcon />
-    </Stack>
+    <Tooltip
+      title={
+        <Stack
+          className="tooltip-note"
+          sx={{
+            display: "flex",
+            gap: 3,
+          }}
+        >
+          <Typography fontSize={13}>{title}</Typography>
+          <Typography fontSize={11}>{description}</Typography>
+          <Link href={`${link}`} sx={{ color: "white" }} fontSize={11}>
+            Read more
+          </Link>
+        </Stack>
+      }
+    >
+      <Stack component={"div"} sx={theme.helperFrameStyle}>
+        <QuestionMarkIcon />
+      </Stack>
+    </Tooltip>
   );
 };
 

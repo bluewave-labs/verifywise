e45b4a9b6f848092cccbaea81a9cb8003cd2ed31
Harsh <harsh@Harsh.local>
2025-06-18 02:11:15 -0400

    fix ssl url for backend

--- Clients/env.vars.ts
+++ Clients/env.vars.ts
@@ -2,7 +2,7 @@ export const ENV_VARs = {
   URL:
     import.meta.env.VITE_APP_API_BASE_URL ??            // keep empty string if set
     (typeof window !== 'undefined'                      // only run in browser
-      ? `${window.location.protocol}//${window.location.hostname}:3000`
+      ? `${window.location.protocol}//${window.location.hostname}${window.location.protocol === 'https:' ? '' : ':3000'}` // use current URL if not set
       : 'http://localhost:3000/'),                      // final Node/SSR fallback
   IS_DEMO_APP: import.meta.env.VITE_IS_DEMO_APP === 'true',
 };

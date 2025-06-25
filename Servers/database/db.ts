efca360b8b6910bb11964a6b3c8dc9b54d65936f
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-06-25 09:45:06 +0330

    Role entity moved to domain.layer

--- Servers/database/db.ts
+++ Servers/database/db.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import { Sequelize } from "sequelize-typescript";
-import { RoleModel } from "../models/role.model";
+import { RoleModel } from "../domain.layer/models/role/role.model";
 import { AssessmentModel } from "../models/assessment.model";
 import { ControlModel } from "../models/control.model";
 import { ControlCategoryModel } from "../models/controlCategory.model";

f5abf18de7beac8d5bcb6e39436097878bef635a
eem <eimon1150@gmail.com>
2025-06-27 01:38:23 -0600

    Resolved table id indexing

--- Clients/src/presentation/components/LinkedRisks/index.tsx
+++ Clients/src/presentation/components/LinkedRisks/index.tsx
@@ -223,7 +223,7 @@ const RiskTableBody: React.FC<TableProps> = ({
                   />
                 </TableCell>
                 <TableCell sx={cellStyle}>
-                  {index + 1}
+                  {page * rowsPerPage + index + 1}
                 </TableCell>
                 <TableCell>
                   {row.risk_name ? row.risk_name : '-'}

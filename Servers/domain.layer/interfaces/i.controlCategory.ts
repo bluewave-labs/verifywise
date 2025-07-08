0a93e7d70b4d32e1a9900ccafb6891ebe85b7f15
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-07-08 14:15:30 +0330

    Adding logging and instance creation to controlCategory controller

--- Servers/domain.layer/interfaces/i.controlCategory.ts
+++ Servers/domain.layer/interfaces/i.controlCategory.ts
@@ -5,6 +5,7 @@ export type IControlCategory = {
   project_id: number; // FK to the project table
   title: string; // gets assigned from the structure
   order_no?: number; // gets assigned from the structure
+  is_demo?: boolean; // indicates if this is a demo control category
   controls?: IControl[];
   created_at?: Date;
 };

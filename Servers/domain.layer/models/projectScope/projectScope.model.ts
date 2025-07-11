b1ba200883c5d2c0091027f71281173638462006
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-07-11 12:51:08 +0330

    ProjectScopeModel re-structured by linting, extends and implements(interfaces)

--- Servers/domain.layer/models/projectScope/projectScope.model.ts
+++ Servers/domain.layer/models/projectScope/projectScope.model.ts
@@ -1,23 +1,13 @@
 import { Column, DataType, Model, Table } from "sequelize-typescript";
-
-export type ProjectScope = {
-  id?: number;
-  assessmentId: number;
-  describeAiEnvironment: string;
-  isNewAiTechnology: boolean;
-  usesPersonalData: boolean;
-  projectScopeDocuments: string;
-  technologyType: string;
-  hasOngoingMonitoring: boolean;
-  unintendedOutcomes: string;
-  technologyDocumentation: string;
-  created_at?: Date;
-};
+import { IProjectScope } from "../../interfaces/i.projectScope";
 
 @Table({
-  tableName: "project_scopes"
+  tableName: "project_scopes",
 })
-export class ProjectScopeModel extends Model<ProjectScope> {
+export class ProjectScopeModel
+  extends Model<ProjectScopeModel>
+  implements IProjectScope
+{
   @Column({
     type: DataType.INTEGER,
     autoIncrement: true,
@@ -27,67 +17,67 @@ export class ProjectScopeModel extends Model<ProjectScope> {
 
   @Column({
     type: DataType.INTEGER,
-    field: "assessment_id"
+    field: "assessment_id",
   })
   assessmentId!: number;
 
   @Column({
     type: DataType.STRING,
-    field: "describe_ai_environment"
+    field: "describe_ai_environment",
   })
   describeAiEnvironment!: string;
 
   @Column({
     type: DataType.BOOLEAN,
-    field: "is_new_ai_technology"
+    field: "is_new_ai_technology",
   })
   isNewAiTechnology!: boolean;
 
   @Column({
     type: DataType.BOOLEAN,
-    field: "uses_personal_data"
+    field: "uses_personal_data",
   })
   usesPersonalData!: boolean;
 
   @Column({
     type: DataType.STRING,
-    field: "project_scope_documents"
+    field: "project_scope_documents",
   })
   projectScopeDocuments!: string;
 
   @Column({
     type: DataType.STRING,
-    field: "technology_type"
+    field: "technology_type",
   })
   technologyType!: string;
 
   @Column({
     type: DataType.BOOLEAN,
-    field: "has_ongoing_monitoring"
+    field: "has_ongoing_monitoring",
   })
   hasOngoingMonitoring!: boolean;
 
   @Column({
     type: DataType.STRING,
-    field: "unintended_outcomes"
+    field: "unintended_outcomes",
   })
   unintendedOutcomes!: string;
 
   @Column({
     type: DataType.STRING,
-    field: "technology_documentation"
+    field: "technology_documentation",
   })
   technologyDocumentation!: string;
 
   @Column({
     type: DataType.BOOLEAN,
     allowNull: false,
-    defaultValue: false
+    defaultValue: false,
   })
   is_demo?: boolean;
 
   @Column({
-    type: DataType.DATE
+    type: DataType.DATE,
   })
   created_at?: Date;
 }

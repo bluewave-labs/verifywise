1226db4197a36e04b9f9069e0bf9ae75248cbe98
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-07-29 13:10:43 +0330

    Logger route added in BE

---
+++ Servers/routes/logger.route.ts
@@ -0,0 +1,22 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/events", async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "Events fetched successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to get events" });
+  }
+});
+
+router.get("/logs", async (req, res) => {
+  try {
+    res.status(200).json({
+      message: "Logs fetched successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to get logs" });
+  }
+});

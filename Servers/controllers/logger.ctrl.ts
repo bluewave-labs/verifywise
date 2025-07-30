ef9f41ad69fd1d48edc33e4b905b1c032421376b
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-07-30 09:25:37 +0330

    Both raw events and logs fetch correctly

--- Servers/controllers/logger.ctrl.ts
+++ Servers/controllers/logger.ctrl.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getEventsQuery } from "../utils/logger.util";
+import { getEventsQuery, getLogsQuery } from "../utils/logger.util";
 import { STATUS_CODE } from "../utils/statusCode.utils";
 
 async function getEvents(req: Request, res: Response) {
@@ -13,6 +13,8 @@ async function getEvents(req: Request, res: Response) {
 
 async function getLogs(req: Request, res: Response) {
   try {
+    const logs = await getLogsQuery();
+    res.status(200).json(STATUS_CODE[200](logs));
   } catch (error) {
     res.status(500).json({ message: "Failed to get logs" });
   }

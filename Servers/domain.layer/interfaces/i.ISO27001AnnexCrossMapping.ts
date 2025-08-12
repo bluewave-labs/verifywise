f1b6d30d7418327ea081873b0bdc5c561fa945f5
Harsh <harsh@Harsh.local>
2025-08-12 16:22:09 -0400

    added interfaces for ISO 27001

--- Servers/domain.layer/interfaces/i.ISO27001AnnexCrossMapping.ts
+++
@@ -1,8 +0,0 @@
-export interface IISO27001AnnexCrossMapping {
-  id?: number;
-  clause_no: number;
-  order_no: number;
-  clause_title: string;
-  relevance: string;
-  iso27001annex_control_id: number;
-}

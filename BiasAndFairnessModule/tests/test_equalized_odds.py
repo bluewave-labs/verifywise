99083bdf40761cf623e80d7a08d22cf0d6ea810e
sermengi <mengiserkan@outlook.com>
2025-08-14 11:14:27 -0400

    Add unit tests for equalized odds metric to validate output accuracy

---
+++ BiasAndFairnessModule/tests/test_equalized_odds.py
@@ -0,0 +1,20 @@
+import unittest
+
+from tests.base_test_metrics import BaseMetricsTestCase
+from src.metrics import equalized_odds
+
+
+class TestEqualizedOdds(BaseMetricsTestCase):
+    def test_equalized_odds_value(self):
+        value = equalized_odds(
+            y_true=self.y_true,
+            y_pred=self.y_pred,
+            protected_attributes=self.protected_attributes,
+        )
+
+        self.assertAlmostEqual(value, 0.306342963063, places=12)
+
+
+if __name__ == "__main__":
+    unittest.main()
+

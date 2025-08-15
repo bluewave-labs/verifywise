b562612189cce96a7452f6d55d3c8930757fad28
Karan Solanki <solan117@uwindsor.ca>
2025-08-14 22:25:40 -0400

    Remove unused tab-related styles from Reporting page styles.

--- Clients/src/presentation/pages/Reporting/styles.ts
+++ Clients/src/presentation/pages/Reporting/styles.ts
@@ -9,34 +9,10 @@ export const styles = {
     color: "#344054",
     fontSize: 13,
   },
-  tabDivider: {
-    borderBottom: 1,
-    borderColor: "divider",
-  },
-  tabList: {
-    minHeight: "20px",
-    "& .MuiTabs-flexContainer": { columnGap: "34px" },
-  },
   reportButtonContainer: {
     display: "flex",
     justifyContent: "flex-end",
     alignItems: "center",
     mb: 2,
-    "& button": {
-      backgroundColor: "#1976d2",
-      color: "#fff",
-      padding: "0",
-      textTransform: "none",
-      "&:hover": {
-        backgroundColor: "#1565c0",
-      },
-      "&:focus": {
-        outline: "none",
-        boxShadow: "0 0 0 0",
-      },
-      "&:active": {
-        backgroundColor: "#013973",
-      },
-    },
   },
 };

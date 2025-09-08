320d21db3ce6fd60eff2d7295a66ce8fc116ad5d
swaleha456 <swaleha2612000@gmail.com>
2025-09-08 11:15:41 -0400

    save-button-possition-fix-in-policy-edit-modal

--- Clients/src/presentation/components/Policies/PolicyDetailsModal.tsx
+++ Clients/src/presentation/components/Policies/PolicyDetailsModal.tsx
@@ -274,7 +274,9 @@ const PolicyDetailModal: React.FC<Props> = ({
 
         <Divider sx={{ my: 2 }} />
 
-        <Stack spacing={4}>
+        <Stack spacing={4} sx={{
+            paddingBottom: "80px", // leaves space so content won't hide under Save button
+          }}>
           <PolicyForm
             formData={formData}
             setFormData={setFormData}
@@ -450,13 +452,16 @@ const PolicyDetailModal: React.FC<Props> = ({
 
         <Box
           sx={{
-            position: "absolute",
-            bottom: 10,
-            right: 10,
-            width: "100%",
+            position: "fixed",            
+            bottom: 0,
+            right: 0,
+            width: 800,                     // same width as Drawer
             p: 2,
+            backgroundColor: "#fff",        // give it a background to overlap content
+            borderTop: "1px solid #E0E0E0", 
             display: "flex",
             justifyContent: "flex-end",
+            zIndex: 1201,                   // above Drawer content
           }}
         >
           <CustomizableButton

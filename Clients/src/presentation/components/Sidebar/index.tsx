f48e6d9992b6c024b36868ef5e21938de3198661
Harsh <harsh@Harsh.local>
2025-09-07 21:43:25 -0400

    fix build failure

--- Clients/src/presentation/components/Sidebar/index.tsx
+++ Clients/src/presentation/components/Sidebar/index.tsx
@@ -38,7 +38,7 @@ import { ReactComponent as Policies } from "../../assets/icons/policies.svg";
 
 /**Adding the training register icon */
 import { ReactComponent as TrainingRegister } from "../../assets/icons/training-register.svg";
-import { ReactComponent as WatchTower } from "../../assets/icons/telescope.svg";
+// import { ReactComponent as WatchTower } from "../../assets/icons/telescope.svg";
 import { ReactComponent as ModelInventory } from "../../assets/icons/list.svg";
 
 import Logo from "../../assets/imgs/logo.png";

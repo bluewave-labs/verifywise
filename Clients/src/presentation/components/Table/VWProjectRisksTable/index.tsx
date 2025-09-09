b924299cca698a857e3b9896456b2a0e39ef7384
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-09-09 14:23:28 +0330

    Fixed svg path

--- Clients/src/presentation/components/Table/VWProjectRisksTable/index.tsx
+++ Clients/src/presentation/components/Table/VWProjectRisksTable/index.tsx
@@ -13,8 +13,8 @@ import {
 import singleTheme from "../../../themes/v1SingleTheme";
 import { useCallback, useMemo, useState, useEffect } from "react";
 import TablePaginationActions from "../../TablePagination";
-import { ReactComponent as SelectorVertical } from "../../assets/icons/selector-vertical.svg";
-import placeholderImage from "../../assets/imgs/empty-state.svg";
+import { ReactComponent as SelectorVertical } from "../../../assets/icons/selector-vertical.svg";
+import placeholderImage from "../../../assets/imgs/empty-state.svg";
 import VWProjectRisksTableHead from "./VWProjectRisksTableHead";
 import VWProjectRisksTableBody from "./VWProjectRisksTableBody";
 

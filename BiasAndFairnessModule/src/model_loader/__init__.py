b46d76f20f2d21df2ebfaadda95206db6b91fb29
sermengi <mengiserkan@outlook.com>
2025-09-15 14:10:18 -0400

    Update inference results path in config.yaml and remove unused model_loader module

--- BiasAndFairnessModule/src/model_loader/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Model loader package for Bias and Fairness Module.
-"""
-
-try:
-    from .model_loader import ModelLoader, load_sklearn_model  # type: ignore
-    __all__ = ['ModelLoader', 'load_sklearn_model']
-except Exception:  # pragma: no cover
-    __all__ = []

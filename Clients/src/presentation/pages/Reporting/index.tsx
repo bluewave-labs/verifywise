4b00e04cc30a6cdd61282d1e5402414d035a3865
gorkem-bwl <gorkem.cetin@bluewavelabs.ca>
2025-09-18 00:19:09 -0400

    Fix frontend build error - correct JSX syntax in NewRisk component

--- Clients/src/presentation/pages/Reporting/index.tsx
+++ Clients/src/presentation/pages/Reporting/index.tsx
@@ -1,12 +1,11 @@
 import { Suspense, lazy, useState, useCallback } from "react";
-import { Stack, Box } from "@mui/material";
+import { Stack } from "@mui/material";
 import PageBreadcrumbs from "../../components/Breadcrumbs/PageBreadcrumbs";
 const GenerateReport = lazy(() => import("./GenerateReport"));
 const ReportLists = lazy(() => import("./Reports"));
 const ReportingHeader = lazy(
   () => import("../../components/Reporting/ReportOverviewHeader")
 );
-import { styles } from "./styles";
 import HelperDrawer from "../../components/Drawer/HelperDrawer";
 import reportingHelpContent from "../../../presentation/helpers/reporting-help.html?raw";
 

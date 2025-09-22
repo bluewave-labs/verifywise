60755088a140173d85cd8d9af7898fae678d5c26
Aryanak47 <aryanbimali45@gmail.com>
2025-09-22 17:50:10 -0400

    - create project status enum - add status in project's interface - update project's model to support status

---
+++ Servers/domain.layer/enums/project-status.enum.ts
@@ -0,0 +1,9 @@
+export enum ProjectStatus {
+  NOT_STARTED = "not_started",
+  IN_PROGRESS = "in_progress", 
+  UNDER_REVIEW = "under_review",
+  COMPLETED = "completed",
+  CLOSED = "closed",
+  ON_HOLD = "on_hold",
+  REJECTED = "rejected"
+}

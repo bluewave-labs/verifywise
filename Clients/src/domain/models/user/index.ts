127c501b8a7e4f1adcf46ada4b1be12fc8adb3a9
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-09-23 11:42:33 +0330

    UserModel class is added

---
+++ Clients/src/domain/models/user/index.ts
@@ -0,0 +1,27 @@
+import { User } from "../../types/User";
+
+export class UserModel {
+  id: number;
+  name: string;
+  surname: string;
+  email: string;
+  password_hash?: string;
+  roleId?: number;
+  created_at?: Date;
+  last_login?: Date;
+
+  constructor(data: User) {
+    this.id = data.id;
+    this.name = data.name;
+    this.surname = data.surname;
+    this.email = data.email;
+    this.password_hash = data.password_hash;
+    this.roleId = data.roleId;
+    this.created_at = data.created_at;
+    this.last_login = data.last_login;
+  }
+
+  static createNewUser(data: User): UserModel {
+    return new UserModel(data);
+  }
+}

35b19da93a1a0d3bb95db7b486d9a70251481fc3
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-09-26 13:17:25 +0330

    SubClauseISO42001Model added

---
+++ Clients/src/domain/models/ISO-42001/subClauseISO42001/subClauseISO42001.model.ts
@@ -0,0 +1,39 @@
+import { Status } from "../../../types/Status";
+
+export class SubClauseISO42001Model {
+  id?: number;
+  implementation_description!: string;
+  evidence_links!: Object[];
+  staus!: Status;
+  owner!: number;
+  reviewer!: number;
+  approver!: number;
+  due_date!: Date;
+  auditor_feedback!: string;
+  subclause_meta_id!: number;
+  projects_frameworks_id!: number;
+  created_at!: Date;
+  is_demo?: boolean;
+
+  constructor(data: SubClauseISO42001Model) {
+    this.id = data.id;
+    this.implementation_description = data.implementation_description;
+    this.evidence_links = data.evidence_links;
+    this.staus = data.staus;
+    this.owner = data.owner;
+    this.reviewer = data.reviewer;
+    this.approver = data.approver;
+    this.due_date = data.due_date;
+    this.auditor_feedback = data.auditor_feedback;
+    this.subclause_meta_id = data.subclause_meta_id;
+    this.projects_frameworks_id = data.projects_frameworks_id;
+    this.created_at = data.created_at;
+    this.is_demo = data.is_demo;
+  }
+
+  static createNewSubClauseISO42001(
+    data: SubClauseISO42001Model
+  ): SubClauseISO42001Model {
+    return new SubClauseISO42001Model(data);
+  }
+}

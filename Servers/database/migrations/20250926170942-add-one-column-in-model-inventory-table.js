a44f9df87ce313e5dddf4c58c3f3006b49cc5844
swaleha456 <swaleha2612000@gmail.com>
2025-09-26 13:24:43 -0400

    added-used-in-projects-dropdown-in-add-new-model

---
+++ Servers/database/migrations/20250926170942-add-one-column-in-model-inventory-table.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const { getTenantHash } = require("../../dist/tools/getTenantHash");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      const organizations = await queryInterface.sequelize.query(
+        `SELECT id FROM organizations;`,
+        { transaction }
+      );
+
+      for (let organization of organizations[0]) {
+        const tenantHash = getTenantHash(organization.id);
+
+        // Add column with TEXT type and default value
+        await queryInterface.sequelize.query(
+          `ALTER TABLE "${tenantHash}".model_inventories
+           ADD COLUMN used_in_projects TEXT NOT NULL DEFAULT '';`,
+          { transaction }
+        );
+      }
+
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      const organizations = await queryInterface.sequelize.query(
+        `SELECT id FROM organizations;`,
+        { transaction }
+      );
+
+      for (let organization of organizations[0]) {
+        const tenantHash = getTenantHash(organization.id);
+
+        await queryInterface.sequelize.query(
+          `ALTER TABLE "${tenantHash}".model_inventories
+           DROP COLUMN used_in_projects;`,
+          { transaction }
+        );
+      }
+
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  }
+};

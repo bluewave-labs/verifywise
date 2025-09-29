037e62c9bea60727f68ea18e07456c6a228252d4
Karan Solanki <karan.solanki69@gmail.com>
2025-09-29 00:39:53 -0400

    refactor(icons): replace MUI `QuestionMark` icon with custom SVG
    - Replaced `@mui/icons-material/QuestionMark` with `questionMark.svg`.
    - Updated import path for the new SVG.

--- Clients/src/presentation/components/Helpers/Guider/index.tsx
+++ Clients/src/presentation/components/Helpers/Guider/index.tsx
@@ -1,5 +1,5 @@
 import { Link, Stack, Tooltip, Typography } from "@mui/material";
-import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import QuestionMarkIcon from "../../../assets/icons/questionMark.svg";
 import { GuiderStyler } from "./style";
 
 const Guider = ({

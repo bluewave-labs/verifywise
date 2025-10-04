8a05e8f6d628438cfe1643fa456347c1e05ef990
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-10-04 12:28:28 +0330

    Evidences -> Evidence

--- Clients/src/presentation/components/Sidebar/index.tsx
+++ Clients/src/presentation/components/Sidebar/index.tsx
@@ -118,7 +118,7 @@ const getMenuItems = (openTasksCount: number): MenuItem[] => [
     path: "/vendors",
   },
   {
-    name: "Evidences",
+    name: "Evidence",
     icon: <FileManager />,
     path: "/file-manager",
   },

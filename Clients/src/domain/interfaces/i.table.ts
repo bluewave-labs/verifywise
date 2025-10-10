3997a0d5cd4db5fd69174f35a98080579622f4f3
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-10-10 22:05:35 +0330

    IAITrustCenterTableProps added as interface

--- Clients/src/domain/interfaces/i.table.ts
+++ Clients/src/domain/interfaces/i.table.ts
@@ -2,3 +2,14 @@ export interface IAITrustCenterTableColumn {
   id: string;
   label: string;
 }
+export interface IAITrustCenterTableProps<T> {
+  data: T[];
+  columns: IAITrustCenterTableColumn[];
+  isLoading?: boolean;
+  paginated?: boolean;
+  emptyStateText?: string;
+  renderRow: (item: T) => React.ReactNode;
+  onRowClick?: (item: T) => void;
+  tableId?: string;
+  disabled?: boolean;
+}

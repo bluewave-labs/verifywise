cec800cafc82e7282c1eae8e3d7252c7b0d87083
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-10-21 10:47:09 +0330

    IRiskCategoriesProps added

--- Clients/src/domain/interfaces/i.risk.ts
+++ Clients/src/domain/interfaces/i.risk.ts
@@ -1,6 +1,14 @@
+import { ProjectRisk } from "../types/ProjectRisk";
+
 export interface IRisk {
   id: number;
   title: string;
   status: string;
   severity: string;
+}
+
+export interface IRiskCategoriesProps {
+  risks: ProjectRisk[];
+  selectedRisk?: ProjectRisk | null;
+  onRiskSelect?: (risk: ProjectRisk) => void;
 }

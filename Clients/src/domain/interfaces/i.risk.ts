ec63957faf934dd3e4b659bae877c62739f33624
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-10-21 10:53:04 +0330

    IRiskFiltersProps added

--- Clients/src/domain/interfaces/i.risk.ts
+++ Clients/src/domain/interfaces/i.risk.ts
@@ -1,4 +1,5 @@
 import { ProjectRisk } from "../types/ProjectRisk";
+import { IFilterState } from "./i.filter";
 
 export interface IRisk {
   id: number;
@@ -11,4 +12,12 @@ export interface IRiskCategoriesProps {
   risks: ProjectRisk[];
   selectedRisk?: ProjectRisk | null;
   onRiskSelect?: (risk: ProjectRisk) => void;
+}
+
+export interface IRiskFiltersProps {
+  risks: ProjectRisk[];
+  onFilterChange: (
+    filteredRisks: ProjectRisk[],
+    activeFilters: IFilterState
+  ) => void;
 }

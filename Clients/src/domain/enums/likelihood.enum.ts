ce1468014629091aa8341cd3e4b83dd342c8a4a9
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-10-22 09:42:25 +0330

    RiskLevelLikelihood enum added

--- Clients/src/domain/enums/likelihood.enum.ts
+++ Clients/src/domain/enums/likelihood.enum.ts
@@ -5,3 +5,11 @@ export enum Likelihood {
   Likely = "Likely",
   AlmostCertain = "Almost Certain",
 }
+
+export enum RiskLevelLikelihood {
+  Rare = 1,
+  Unlikely = 2,
+  Possible = 3,
+  Likely = 4,
+  AlmostCertain = 5,
+}

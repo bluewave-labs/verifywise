9427c32c9886658c8bb901dbda14c6ad29ca02ac
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2025-10-24 11:03:21 +0330

    ICustomStepWrapperProps added

--- Clients/src/domain/interfaces/i.customs.ts
+++ Clients/src/domain/interfaces/i.customs.ts
@@ -1,5 +1,11 @@
+import { TooltipRenderProps } from "react-joyride";
+
 export interface ICustomStepProps {
   header?: string;
   body: string;
   icon?: React.ReactNode;
+}
+
+export interface ICustomStepWrapperProps extends TooltipRenderProps {
+  content: ICustomStepProps;
 }

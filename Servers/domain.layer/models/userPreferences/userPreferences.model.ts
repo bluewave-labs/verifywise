11a3f203b67f274abdb710520c7e4a9b29ddba15
Rachana Basnet <rachanabasnet19@gmail.com>
2025-11-06 19:15:37 -0500

    create interface, model, controllers, utils, and route to get, create, and update user preferences

---
+++ Servers/domain.layer/models/userPreferences/userPreferences.model.ts
@@ -0,0 +1,109 @@
+import {
+  Model,
+  Table,
+  Column,
+  DataType,
+  ForeignKey,
+} from "sequelize-typescript";
+import { UserModel } from "../user/user.model";
+import { IUserPreferences } from "../../interfaces/i.userPreferences";
+import { UserDateFormat } from "../../enums/user-preferences.enum";
+import { ValidationException } from "../../exceptions/custom.exception";
+
+@Table({
+  tableName: "user_preferences",
+})
+export class UserPreferencesModel
+  extends Model<UserPreferencesModel>
+  implements IUserPreferences
+{
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id!: number;
+
+  @ForeignKey(() => UserModel)
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  user_id!: number;
+
+  @Column({
+    type: DataType.ENUM(...Object.values(UserDateFormat)),
+    defaultValue: "DD-MM-YYYY",
+    allowNull: false,
+  })
+  date_format!: UserDateFormat;
+
+  /**
+   * Create new user preferences
+   * @param user_id - ID of the user
+   * @param date_format - Preferred date format
+   * @returns Newly created UserPreferences instance
+   */
+  static async createNewUserPreferences(
+    user_id: number,
+    date_format: UserDateFormat,
+  ): Promise<UserPreferencesModel> {
+    const userPreferencesData = new UserPreferencesModel();
+
+    const validDateFormats = Object.values(UserDateFormat);
+    if (!validDateFormats.includes(date_format)) {
+      throw new ValidationException(
+        `Invalid date format. Must be one of: ${validDateFormats.join(", ")}`,
+      );
+    }
+
+    userPreferencesData.user_id = user_id;
+    userPreferencesData.date_format = date_format;
+
+    const userPreferences =
+      await UserPreferencesModel.create(userPreferencesData);
+    return userPreferences;
+  }
+
+  /**
+   * Update user preferences
+   * @param updatedUserPreferences - Updated user preferences data
+   * @returns void
+   */
+  async updateUserPreferences(updatedUserPreferences: {
+    date_format?: UserDateFormat;
+  }): Promise<void> {
+    if (updatedUserPreferences.date_format !== undefined) {
+      this.date_format = updatedUserPreferences.date_format;
+    }
+
+    await this.validateUserPreferences();
+
+    await this.save();
+  }
+
+  /**
+   * Validate user preferences
+   * @returns void
+   */
+  async validateUserPreferences(): Promise<void> {
+    const validDateFormats = Object.values(UserDateFormat);
+    if (!validDateFormats.includes(this.date_format)) {
+      throw new ValidationException(
+        `Invalid date format. Must be one of: ${validDateFormats.join(", ")}`,
+      );
+    }
+  }
+
+  /**
+   * Convert UserPreferences instance to JSON
+   * @returns IUserPreferences object
+   */
+  toJSON(): IUserPreferences {
+    return {
+      id: this.id,
+      user_id: this.user_id,
+      date_format: this.date_format,
+    };
+  }
+}
---
+++ Servers/utils/userPreference.utils.ts
@@ -0,0 +1,78 @@
+import { Transaction } from "sequelize";
+import { sequelize } from "../database/db";
+import { UserPreferencesModel } from "../domain.layer/models/userPreferences/userPreferences.model";
+
+export const getPreferencesByUserQuery = async (
+  userId: number,
+): Promise<UserPreferencesModel | null> => {
+  try {
+    const [preference] = await sequelize.query(
+      `SELECT * FROM user_preferences WHERE user_id = :id`,
+      {
+        replacements: { id: userId },
+        mapToModel: true,
+        model: UserPreferencesModel,
+      },
+    );
+
+    if (!preference) {
+      return null;
+    }
+    return preference;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createNewUserPreferencesQuery = async (
+  data: Omit<UserPreferencesModel, "id">,
+  transaction: Transaction,
+): Promise<UserPreferencesModel> => {
+  const result = await sequelize.query(
+    `INSERT INTO user_preferences (user_id, date_format) VALUES (:user_id, :date_format) RETURNING *`,
+    {
+      replacements: {
+        user_id: data.user_id,
+        date_format: data.date_format,
+      },
+      mapToModel: true,
+      model: UserPreferencesModel,
+      transaction,
+    },
+  );
+  return result[0];
+};
+
+export const updateUserPreferencesByIdQuery = async (
+  id: number,
+  data: Partial<UserPreferencesModel>,
+  transaction: Transaction,
+): Promise<UserPreferencesModel | null> => {
+  const updatedData: Partial<Record<keyof UserPreferencesModel, any>> = {};
+  const setClause = ["date_format"]
+    .filter((f) => {
+      if (
+        data[f as keyof UserPreferencesModel] !== undefined &&
+        data[f as keyof UserPreferencesModel]
+      ) {
+        updatedData[f as keyof UserPreferencesModel] =
+          data[f as keyof UserPreferencesModel];
+        return true;
+      }
+    })
+    .map((f) => `${f} = :${f}`)
+    .join(", ");
+
+  const query = `UPDATE user_preferences SET ${setClause} WHERE id = :id RETURNING *;`;
+
+  updatedData.id = id;
+
+  const result = await sequelize.query(query, {
+    replacements: updatedData,
+    mapToModel: true,
+    model: UserPreferencesModel,
+    transaction,
+  });
+
+  return result[0];
+};

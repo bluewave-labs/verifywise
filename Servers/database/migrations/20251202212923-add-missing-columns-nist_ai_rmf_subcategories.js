3ca6de68f9d34d952aaaf84991c4697477a377ab
Harsh <harsh@Harsh-2.local>
2025-12-02 16:34:12 -0500

    add missing columns for nist_ai_rmf_subcategories

---
+++ Servers/database/migrations/20251202212923-add-missing-columns-nist_ai_rmf_subcategories.js
@@ -0,0 +1,57 @@
+'use strict';
+const { getTenantHash } = require("../../dist/tools/getTenantHash");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      const organizations = await queryInterface.sequelize.query(
+        `SELECT id FROM organizations;`,
+        { transaction }
+      );
+
+      for (const org of organizations[0]) {
+        const tenantHash = getTenantHash(org.id);
+        await queryInterface.sequelize.query(
+          `
+            ALTER TABLE "${tenantHash}".nist_ai_rmf_subcategories
+            ADD COLUMN subcategory_meta_id INTEGER,
+            ADD COLUMN projects_frameworks_id INTEGER NOT NULL REFERENCES "${tenantHash}".projects_frameworks (id);
+          `,
+          { transaction }
+        );
+      }
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      const organizations = await queryInterface.sequelize.query(
+        `SELECT id FROM organizations;`,
+        { transaction }
+      );
+
+      for (const org of organizations[0]) {
+        const tenantHash = getTenantHash(org.id);
+        await queryInterface.sequelize.query(
+          `
+          ALTER TABLE "${tenantHash}".nist_ai_rmf_subcategories
+          DROP COLUMN IF EXISTS subcategory_meta_id,
+          DROP COLUMN IF EXISTS projects_frameworks_id;
+          `,
+          { transaction }
+        );
+      }
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  }
+};

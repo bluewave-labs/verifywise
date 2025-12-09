bf61fdb0c2e0504ef9cda503f3b257cb5e2984f7
Inna Glamazda <innaglamazda@Mac.lan>
2025-12-09 15:37:42 -0500

    Deleted unnecessary interface file

--- Clients/src/domain/interfaces/i.approvalWorkflow.ts
+++
@@ -1,7 +0,0 @@
-// export interface ICreateApprovalWorkflowProps {
-//   isOpen: boolean;
-//   setIsOpen: (isOpen: boolean) => void;
-//   onSuccess?: (data: ICreateApprovalWorkflowFormValues) => void;
-//   initialData?: IWorkflow;
-//   mode?: "create" | "edit";
-// }

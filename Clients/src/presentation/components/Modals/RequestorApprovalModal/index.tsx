aaaceebb3275badf9c6bacb0adbd9566b731f6d9
Inna Glamazda <innaglamazda@Mac.lan>
2025-12-10 16:40:11 -0500

    Added menuGroups to useEffect dependencies

--- Clients/src/presentation/components/Modals/RequestorApprovalModal/index.tsx
+++ Clients/src/presentation/components/Modals/RequestorApprovalModal/index.tsx
@@ -237,7 +237,7 @@ const RequestorApprovalModal: FC<IRequestorApprovalProps> = ({
                 setSelectedItem(firstItem);
             }
         }
-    }, []);
+    }, [menuGroups]);
 
     return (
         <StandardModal

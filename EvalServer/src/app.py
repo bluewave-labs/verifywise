e8777b898d580bddb2ee749bf807bdc608bf1031
EfeAcar6431 <efeacar2705@gmail.com>
2026-01-02 18:49:47 -0500

    changed app to app:app in uvicorn command

--- EvalServer/src/app.py
+++ EvalServer/src/app.py
@@ -71,4 +71,4 @@ app.include_router(evaluation_logs, tags=["Evaluation Logs & Monitoring"])
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("LLM_EVALS_PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
